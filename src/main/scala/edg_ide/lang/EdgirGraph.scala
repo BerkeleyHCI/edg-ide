eaf9963dc0ca15742a85e26cf84c8bc411383d7c
Richard Lin <elpato25@gmail.com>
2020-12-03 23:14:28 -0800

    wiiiiiiiiip

--- src/main/scala/edg_ide/lang/EdgirGraph.scala
+++ src/main/scala/edg_ide/lang/EdgirGraph.scala
@@ -1,6 +1,7 @@
 package edg_ide
 
 import edg.elem.elem
+import edg.expr.expr
 import edg.ref.ref
 
 
@@ -22,18 +23,27 @@ object EdgirGraph {
   def blockLikeToNode(blockLike: elem.BlockLike, lib: EdgirLibrary): EdgirNode = {
     blockLike.`type` match {
       case elem.BlockLike.Type.Hierarchy(block) =>
+        // Convert contained ports and blocks to HGraph/Edgir* objects
         val portMembers: Map[String, EdgirNodeMember] =
           block.ports.mapValues(port => portLikeToPort(port, lib))
         val blockMembers: Map[String, EdgirNodeMember] =
           block.blocks.mapValues(subblock => blockLikeToNode(subblock, lib))
-        (portMembers.toSeq ++ blockMembers.toSeq).groupBy(_._1).map { case (name, pairs) =>
+
+        // Unify the namespace into a member namespace
+        val allMembers = (portMembers.toSeq ++ blockMembers.toSeq).groupBy(_._1).map { case (name, pairs) =>
           pairs match {
-            case (_, value) :: Nil => value
+            case (_, value) :: Nil => (name -> value)
             case _ => throw new Exception(s"block contains conflicting members with name $name")
           }
         }
-        val allMembers: Map[String, EdgirNodeMember] = Map()
-        val edges: Seq[EdgirEdge] = Seq()  // TODO implement me
+
+        // Read edges from constraints
+        val edges: Seq[EdgirEdge] = block.constraints.collect { case (name, constr) =>
+          constr.expr match {
+            case expr.ValueExpr.Expr.Connected(connect) =>
+            case expr.ValueExpr.Expr.Exported(export) =>
+          }
+        }
         EdgirNode(blockLike, allMembers, edges)
       case elem.BlockLike.Type.LibElem(block) =>
         // TODO implement me
