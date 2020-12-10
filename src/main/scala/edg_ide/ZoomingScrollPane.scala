e03cdec4ca368e22019cc07a32dcfebd3bcf1325
Richard Lin <elpato25@gmail.com>
2020-12-10 01:13:40 -0800

    mouse relative zooming

--- src/main/scala/edg_ide/ZoomingScrollPane.scala
+++ src/main/scala/edg_ide/ZoomingScrollPane.scala
@@ -3,6 +3,7 @@ package edg_ide
 import com.intellij.ui.components.JBScrollPane
 
 import java.awt.event.MouseWheelEvent
+import java.awt.Point
 import javax.swing.JComponent
 
 
@@ -20,9 +21,18 @@ trait ZoomingScrollPane extends JBScrollPane {
   override protected def processMouseWheelEvent(e: MouseWheelEvent) {
     val zoomable = getViewport.getView.asInstanceOf[Zoomable]  // crash otherwise
 
-    val newZoom = zoomable.getZoom * Math.pow(1.1, -1 * e.getPreciseWheelRotation)
-    zoomable.setZoom(newZoom.toFloat)
+    val zoomFactor = Math.pow(1.1, -1 * e.getPreciseWheelRotation)
+    zoomable.setZoom((zoomable.getZoom * zoomFactor).toFloat)
     zoomable.revalidate()
     zoomable.repaint()
+
+    // https://stackoverflow.com/questions/13155382/jscrollpane-zoom-relative-to-mouse-position
+    // to keep the mouse over the same position in pre-scaled space
+    val viewPos = getViewport.getViewPosition
+    val newViewPos = new Point(
+      (e.getPoint.x * (zoomFactor - 1) + zoomFactor * viewPos.x).toInt,
+      (e.getPoint.y * (zoomFactor - 1) + zoomFactor * viewPos.y).toInt
+    )
+    getViewport.setViewPosition(newViewPos)
   }
 }
