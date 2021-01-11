d1fe3aba1277d6c3aef1a0880c3810f0570aab01
Richard Lin <elpato25@gmail.com>
2021-01-10 19:04:52 -0800

    refactoring

--- src/main/scala/edg_ide/SplitFileEditor.scala
+++ src/main/scala/edg_ide/SplitFileEditor.scala
@@ -169,64 +169,6 @@ class SplitFileEditor(private val textEditor: TextEditor, private val file: Virt
     fileInputStream.close()
   }
 
-  val psiElementGenerator = PyElementGenerator.getInstance(getEditor.getProject)
-
-  // Return all siblings (including itself) of a PsiElement
-  def psiSiblings(element: PsiElement): Seq[PsiElement] = element match {
-    case element: PsiElement => Seq(element) ++ psiSiblings(element.getNextSibling)
-    case _ => Seq()  // null case
-  }
-
-  // Returns whether the two Seq of PsiElements produce text matches (individual element-wise)
-  def psiTextMatches(seq1: Seq[PsiElement], seq2: Seq[PsiElement]): Boolean = {  // TODO better match impl?
-    seq1.lengthCompare(seq2.length) == 0 && (seq1.zip(seq2).map { case (val1, val2) =>
-      val1.textMatches(val2)
-    }.forall(x => x))
-  }
-
-  /**
-    * For a PyReferenceExpression of the form self.something, return Some("something"), or None
-    */
-  def psiSelfReference(element: PyReferenceExpression): Option[String] = element match {
-    case element: PyReferenceExpression =>
-      val refElement = psiElementGenerator.createExpressionFromText(LanguageLevel.forElement(element),
-        "self.placeholder"
-      ).asInstanceOf[PyReferenceExpression]
-
-      val elemSiblings = psiSiblings(element.getChildren()(0))
-      val refSiblings = psiSiblings(refElement.getChildren()(0))
-
-      if (psiTextMatches(elemSiblings.slice(0, elemSiblings.length - 1),
-        refSiblings.slice(0, refSiblings.length - 1))) {
-        Some(elemSiblings.last.getText)
-      } else {
-        None
-      }
-    case _ => None
-  }
-
-  /**
-    * For a PyTargetExpression of the form self.something, return Some("something"), or None
-    */
-  def psiSelfTarget(element: PyTargetExpression): Option[String] = element match {
-    case element: PyTargetExpression =>
-      val refElement = psiElementGenerator.createExpressionFromText(LanguageLevel.forElement(element),
-        "self.placeholder"  // TODO: should actually use an assignment so this is a PyTargetExpression?
-      ).asInstanceOf[PyReferenceExpression]
-
-      val elemSiblings = psiSiblings(element.getChildren()(0))
-      val refSiblings = psiSiblings(refElement.getChildren()(0))
-
-      if (psiTextMatches(elemSiblings.slice(0, elemSiblings.length - 1),
-        refSiblings.slice(0, refSiblings.length - 1))) {
-        Some(elemSiblings.last.getText)
-      } else {
-        None
-      }
-    case _ =>
-      None
-  }
-
   /**
     * Selects the block diagram element associated with the PSI element, in both the block diagram and tree views.
     */
@@ -242,11 +184,11 @@ class SplitFileEditor(private val textEditor: TextEditor, private val file: Virt
     }
 
     val referenceOpt = PsiTreeUtil.getParentOfType(element, classOf[PyReferenceExpression]) match {
-      case expr: PyReferenceExpression => psiSelfReference(expr)
+      case expr: PyReferenceExpression => PsiUtils.psiSelfReference(getEditor.getProject, expr)
       case _ => None
     }
     val targetOpt = PsiTreeUtil.getParentOfType(element, classOf[PyTargetExpression]) match {
-      case expr: PyTargetExpression => psiSelfTarget(expr)
+      case expr: PyTargetExpression => PsiUtils.psiSelfTarget(getEditor.getProject, expr)
       case _ => None
     }
 
