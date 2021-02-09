1d130ae871bd052030ba3db352476ed15124f5ab
Richard Lin <elpato25@gmail.com>
2021-02-08 17:42:33 -0800

    remove splitfileeditor

--- src/main/scala/edg_ide/actions/NavigateToBlockAction.scala
+++ src/main/scala/edg_ide/actions/NavigateToBlockAction.scala
@@ -29,4 +29,81 @@ class NavigateToBlockAction() extends AnAction() {
         notificationGroup.createNotification(msg, NotificationType.WARNING).notify(event.getProject)
     }
   }
+
+  /**
+    * Selects the block diagram element associated with the PSI element, in both the block diagram and tree views.
+    * Original from SplitFileEditor before that was deleted
+    */
+//  def selectFromPsi(element: PsiElement) {
+//    val containingClass = PsiTreeUtil.getParentOfType(element, classOf[PyClass]) match {
+//      case null =>
+//        notificationGroup.createNotification(
+//          s"No encapsulating class of selection",
+//          NotificationType.WARNING)
+//            .notify(getEditor.getProject)
+//        return
+//      case pyClass: PyClass => pyClass.getNameIdentifier.getText
+//    }
+//
+//    val referenceOpt = PsiTreeUtil.getParentOfType(element, classOf[PyReferenceExpression]) match {
+//      case expr: PyReferenceExpression => PsiUtils.psiSelfReference(getEditor.getProject, expr)
+//      case _ => None
+//    }
+//    val targetOpt = PsiTreeUtil.getParentOfType(element, classOf[PyTargetExpression]) match {
+//      case expr: PyTargetExpression => PsiUtils.psiSelfTarget(getEditor.getProject, expr)
+//      case _ => None
+//    }
+//
+//    val name = referenceOpt.getOrElse(targetOpt.getOrElse {
+//      notificationGroup.createNotification(
+//        s"No reference of form self.(element) selected",
+//        NotificationType.WARNING)
+//          .notify(getEditor.getProject)
+//      return
+//    } )
+//
+//    if (design.contents.isDefined) {
+//      // First, try searching for the selected element in the currently selected block
+//      val startingBlock = EdgirUtils.ResolvePath(design.contents.get, selectedPath) match {
+//        case Some(startingBlock: elem.HierarchyBlock) => startingBlock
+//        case startingBlock =>
+//          println(s"Failed to resolve current path $selectedPath, got $startingBlock")  // TODO use logging infra
+//          return
+//      }
+//
+//      val startingSuperclass = EdgirUtils.SimpleSuperclass(startingBlock.superclasses)
+//      if (startingSuperclass == containingClass) {
+//        if (startingBlock.blocks.contains(name) || startingBlock.ports.contains(name)) {
+//          selectByPath(selectedPath ++ Seq(name))
+//          return
+//        } else {
+//          println(s"Failed to resolve selected PSI $name at selected path $selectedPath")  // TODO use logging infra
+//          return
+//        }
+//      }
+//
+//      // Next, try searching for the selected element in the parent block, to support multiple navigation operations
+//      // which need sibling-level search
+//      if (selectedPath.nonEmpty) {
+//        val parentPath = selectedPath.slice(0, selectedPath.length - 1)
+//        val parentBlock = EdgirUtils.ResolvePath(design.contents.get, parentPath) match {
+//          case Some(parentBlock: elem.HierarchyBlock) => parentBlock
+//          case parentBlock =>
+//            println(s"Failed to resolve current parent path $parentPath, got $parentBlock")  // TODO use logging infra
+//            return
+//        }
+//
+//        val parentSuperclass = EdgirUtils.SimpleSuperclass(parentBlock.superclasses)
+//        if (parentSuperclass == containingClass) {  // first try searching in the selected block
+//          if (parentBlock.blocks.contains(name) || parentBlock.ports.contains(name)) {
+//            selectByPath(parentPath ++ Seq(name))
+//            return
+//          } else {
+//            println(s"Failed to resolve selected PSI $name at parent path $selectedPath")  // TODO use logging infra
+//            return
+//          }
+//        }
+//      }
+//    }
+//  }
 }
