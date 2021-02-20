1c624e6e0e5b7f164408b9d3a4265394b3a70dbf
nikhilJain17 <nikhil.jain0987@gmail.com>
2021-02-19 19:51:10 -0800

    parsing into data structures ready for drawing

--- src/main/scala/edg_ide/ui/KicadParser.scala
+++ src/main/scala/edg_ide/ui/KicadParser.scala
@@ -1,5 +1,5 @@
 package edg_ide.ui
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 import scala.io.Source
 
 class KicadParser {
@@ -32,8 +32,153 @@ class KicadParser {
     println("done parse")
 
     println("printing dump")
+    var i = 0
+
+    var kicadComponents = new ArrayBuffer[KicadComponent]
+
     for (p <- parsed.values) {
-      println(p)
+
+      // Iterate over the parsed values, seeing if they are a rect / line, and if so, parse into appropriate data structure
+      // TODO this code sucks
+      p match {
+        case SList(values) =>
+          if (values.length > 3) {
+            values.head match {
+              case atom: Atom =>
+                if (atom.symbol.equalsIgnoreCase("fp_line")) {
+                  // match on "FSilk" for lines
+                  val line = values.filter {
+                    case a:Atom => false
+                    case s: SList =>
+                      if (s.values.length == 2) {
+                        val first = s.values.head
+                        val second = s.values.tail.head
+                        if (first.isInstanceOf[Atom] && second.isInstanceOf[Atom]) {
+                          second.asInstanceOf[Atom].symbol.equalsIgnoreCase("F.SilkS")
+                        }
+                        else {
+                          false
+                        }
+                      }
+                      else {
+                        false
+                      }
+                  }
+                  if (line.length == 1) {
+                    // We have a valid line, need to get x0, y0, x1, y1
+                    // Assume file is well formed -- i.e. contains "start", "end"
+                    val startPos = values.filter {
+                      case s:SList =>
+                        if (s.values.length == 3) {
+                          val first = s.values.head
+                          first match {
+                            case atom1: Atom =>
+                              atom1.symbol.equalsIgnoreCase("start")
+                            case _ =>
+                              false
+                          }
+                        }
+                        else {
+                          false
+                        }
+                      case _ => false
+                    }.head
+
+                    val endPos = values.filter {
+                      case s:SList =>
+                        if (s.values.length == 3) {
+                          val first = s.values.head
+                          first match {
+                            case atom1: Atom =>
+                              atom1.symbol.equalsIgnoreCase("end")
+                            case _ =>
+                              false
+                          }
+                        }
+                        else {
+                          false
+                        }
+                      case _ => false
+                    }.head
+
+                    // TODO jesus christ
+                    val x0 = startPos.asInstanceOf[SList].values.tail.head.asInstanceOf[Atom].symbol.toFloat
+                    val y0 = startPos.asInstanceOf[SList].values.tail.tail.head.asInstanceOf[Atom].symbol.toFloat
+
+                    val x1 = endPos.asInstanceOf[SList].values.tail.head.asInstanceOf[Atom].symbol.toFloat
+                    val y1 = endPos.asInstanceOf[SList].values.tail.tail.head.asInstanceOf[Atom].symbol.toFloat
+
+                    kicadComponents.addOne(new Line(x0, y0, x1, y1))
+                  }
+                }
+                else if (atom.symbol.equalsIgnoreCase("pad")) {
+                  // match on "rect" for rectangles
+                  val rect = values.filter {
+                    case a:Atom => a.symbol.equalsIgnoreCase("rect")
+                    case _ => false
+                  }
+                  if (rect.length == 1) {
+                    // We have a valid rect, parse params
+                    val startPos = values.filter {
+                      case s:SList =>
+                        if (s.values.length == 3) {
+                          val first = s.values.head
+                          first match {
+                            case atom1: Atom =>
+                              atom1.symbol.equalsIgnoreCase("at")
+                            case _ =>
+                              false
+                          }
+                        }
+                        else {
+                          false
+                        }
+                      case _ => false
+                    }.head
+
+                    val size = values.filter {
+                      case s:SList =>
+                        if (s.values.length == 3) {
+                          val first = s.values.head
+                          first match {
+                            case atom1: Atom =>
+                              atom1.symbol.equalsIgnoreCase("size")
+                            case _ =>
+                              false
+                          }
+                        }
+                        else {
+                          false
+                        }
+                      case _ => false
+                    }.head
+
+                    // TODO jesus christ
+                    val x0 = startPos.asInstanceOf[SList].values.tail.head.asInstanceOf[Atom].symbol.toFloat
+                    val y0 = startPos.asInstanceOf[SList].values.tail.tail.head.asInstanceOf[Atom].symbol.toFloat
+
+                    val width = size.asInstanceOf[SList].values.tail.head.asInstanceOf[Atom].symbol.toFloat
+                    val height = size.asInstanceOf[SList].values.tail.tail.head.asInstanceOf[Atom].symbol.toFloat
+
+                    kicadComponents.addOne(new Rectangle(x0, y0, width, height))
+
+                  }
+
+                }
+
+              case _ =>
+
+            }
+          }
+
+        case _ =>
+
+      }
+
+      for (k <- kicadComponents) {
+        println(k)
+      }
+
 //      case p: Atom => println(p)
 //      case p: SList => println("")
     }
@@ -46,8 +191,8 @@ class KicadParser {
 
   // Kicad IR
   sealed trait KicadComponent
-  case class Rectangle(x:Int, y:Int, width:Int, height:Int) extends KicadComponent
-  case class Line(x0:Int, y0:Int, x1:Int, y1:Int) extends KicadComponent
+  case class Rectangle(x:Float, y:Float, width:Float, height:Float) extends KicadComponent
+  case class Line(x0:Float, y0:Float, x1:Float, y1:Float) extends KicadComponent
 
   /*
   Source: https://github.com/ZenBowman/sexpr
