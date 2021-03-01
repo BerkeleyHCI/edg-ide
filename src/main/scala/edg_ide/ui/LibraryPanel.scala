d46b77d4e9878c24145bff1f192f0620986f1d3d
Richard Lin <elpato25@gmail.com>
2021-02-28 19:34:28 -0800

    caret insert ops

--- src/main/scala/edg_ide/ui/LibraryPanel.scala
+++ src/main/scala/edg_ide/ui/LibraryPanel.scala
@@ -2,21 +2,26 @@ package edg_ide.ui
 
 import com.intellij.lang.LanguageNamesValidation
 import com.intellij.openapi.command.WriteCommandAction.writeCommandAction
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.fileEditor.{FileEditorManager, TextEditor}
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
 import com.intellij.pom.Navigatable
+import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.ui.{JBSplitter, TreeTableSpeedSearch}
 import com.intellij.ui.components.{JBScrollPane, JBTextArea}
 import com.intellij.ui.treeStructure.treetable.TreeTable
 import com.jetbrains.python.PythonLanguage
-import com.jetbrains.python.psi.{LanguageLevel, PyAssignmentStatement, PyElementGenerator, PyFunction}
+import com.jetbrains.python.psi.{LanguageLevel, PyAssignmentStatement, PyClass, PyElementGenerator, PyFunction, PyStatementList}
 import com.jetbrains.python.psi.search.PyClassInheritorsSearch
 import edg.wir
 import edg.ref.ref
+import edg_ide.actions.InsertBlockAction
 import edg_ide.{EdgirUtils, PsiUtils}
 import edg_ide.swing.{EdgirLibraryTreeNode, EdgirLibraryTreeTableModel}
-import edg_ide.util.ExceptionNotifyImplicits.{ExceptBoolean, ExceptErrorable, ExceptSeq}
-import edg_ide.util.{DesignAnalysisUtils, exceptable, exceptionNotify}
+import edg_ide.util.ExceptionNotifyImplicits.{ExceptBoolean, ExceptErrorable, ExceptNotify, ExceptSeq}
+import edg_ide.util.{DesignAnalysisUtils, exceptable, exceptionNotify, requireExcept}
 
 import java.awt.BorderLayout
 import java.awt.event.{ActionEvent, MouseAdapter, MouseEvent}
@@ -51,11 +56,76 @@ class LibraryBlockPopupMenu(path: ref.LibraryPath, project: Project) extends JPo
 
 
   // Edit actions
+  private val caretPsiElement = exceptable {
+    val contextPsiFile = contextPyClass.exceptError.getContainingFile.exceptNull("no file")
+    val editors = FileEditorManager.getInstance(project)
+        .getAllEditors(contextPsiFile.getVirtualFile).toSeq
+        .exceptEmpty("file not open")
+    requireExcept(editors.length == 1, "multiple editors open")
+    val contextEditor = editors.head.instanceOfExcept[TextEditor]("not a text editor")
+    val contextOffset = contextEditor.getEditor.getCaretModel.getOffset
+    val caretPsiElement = contextPsiFile.findElementAt(contextOffset).exceptNull("invalid caret position")
+
+    val caretPsiClass = PsiTreeUtil.getParentOfType(caretPsiElement, classOf[PyClass])
+        .exceptNull("caret not in a class")
+    requireExcept(caretPsiClass == contextPyClass.exceptError, s"caret not in $contextPyName")
+
+    val caretPsiContainingList = caretPsiElement.getParent
+        .instanceOfExcept[PyStatementList](s"caret invalid for insertion")
+    val caretPsiContainingFunction = caretPsiContainingList.getParent
+        .instanceOfExcept[PyFunction]("caret not in a function")
+    requireExcept(InsertBlockAction.VALID_FUNCTION_NAMES.contains(caretPsiContainingFunction.getName),
+      s"caret function ${caretPsiContainingFunction.getName} invalid for block insertion")
+
+    caretPsiElement
+  }
+  private val caretFileLine = exceptable{
+    PsiUtils.fileNextLineOf(caretPsiElement.exceptError, project).exceptError
+  }.mapToString(identity)
+
+  private val insertAtCaretItem = PopupMenuUtils.MenuItemFromErrorable(
+    caretPsiElement, s"Insert $pathName at $contextPyName caret ($caretFileLine)") { caretPsiElement =>
+      exceptionNotify("edg_ide.LibraryBlockPopupMenu", project) {
+        val caretPsiContainingList = caretPsiElement.getParent
+            .instanceOfExcept[PyStatementList](s"caret invalid for insertion")
+        val caretPsiContainingFunction = caretPsiContainingList.getParent
+            .instanceOfExcept[PyFunction]("caret not in a function")
+
+        val psiElementGenerator = PyElementGenerator.getInstance(project)
+
+        val selfName = caretPsiContainingFunction.getParameterList.getParameters.toSeq
+            .exceptEmpty(s"caret function ${caretPsiContainingFunction.getName} has no self")
+            .head.getName
+        val contextAttributeNames = contextPyClass.exceptError.getInstanceAttributes.toSeq.map(_.getName)
+
+        PopupUtils.createStringEntryPopup("Block Name", project) { targetName => exceptable {
+          LanguageNamesValidation.isIdentifier(PythonLanguage.getInstance(), targetName)
+              .exceptFalse("not an identifier")
+          contextAttributeNames.contains(targetName)
+              .exceptTrue(s"attribute already exists in $contextPyName")
+
+          val newAssign = psiElementGenerator.createFromText(LanguageLevel.forElement(caretPsiElement),
+            classOf[PyAssignmentStatement],
+            s"$selfName.$targetName = $selfName.Block($libPyName())"
+          )
+
+          writeCommandAction(project).withName(s"Insert $pathName at $contextPyName caret ($caretFileLine)").run(() => {
+            val added = caretPsiContainingList.addAfter(newAssign, caretPsiElement)
+            added match {
+              case added: Navigatable => added.navigate(true)
+              case _ =>  // ignored
+            }
+          })
+        }}
+      }
+  }
+  add(insertAtCaretItem)
+
   private val insertionFunctions = exceptable {
     DesignAnalysisUtils.findInsertionPoints(contextPyClass.exceptError, project).exceptError
   }
   PopupMenuUtils.MenuItemsFromErrorableSeq(insertionFunctions,
-    errMsg => s"Insert $pathName into ${contextPyName} ($errMsg)",
+    errMsg => s"Insert $pathName into $contextPyName ($errMsg)",
     {fn: PyFunction =>
       val fileLine = PsiUtils.fileNextLineOf(fn.getStatementList.getLastChild, project).mapToString(identity)
       s"Insert $pathName at ${contextPyName}.${fn.getName} ($fileLine)"}) { fn =>
