1bb2a8287d5e7eaded32115476b200602fba53ea
nikhilJain17 <nikhil.jain0987@gmail.com>
2021-03-19 16:19:59 -0700

    moved KicadVizPanel to its own file

--- src/main/scala/edg_ide/ui/BlockVisualizerPanel.scala
+++ src/main/scala/edg_ide/ui/BlockVisualizerPanel.scala
@@ -385,41 +385,6 @@ class LibraryPanel() extends JPanel {
 
 }
 
-class KicadVizPanel() extends JPanel with MouseWheelListener {
-  // State
-  //
-
-  private var library: wir.Library = new wir.EdgirLibrary(schema.Library())
-  private var kicadFile:String = "hello"
-
-  // GUI Components
-  //
-  private val splitter = new JBSplitter(false, 0.5f, 0.1f, 0.9f)
-
-  // TODO placeholder, add custom tree models here
-  private val libraryTree = new TreeTable(new EdgirLibraryTreeTableModel(library))
-  libraryTree.setShowColumns(true)
-  private val libraryTreeScrollPane = new JBScrollPane(libraryTree)
-  splitter.setFirstComponent(libraryTreeScrollPane)
-
-  private val visualizer = new KicadVizDrawPanel()
-  visualizer.offset = (this.libraryTreeScrollPane.getWidth * 1.2).asInstanceOf[Int]
-  splitter.setSecondComponent(visualizer)
-
-  setLayout(new BorderLayout())
-  add(splitter)
-
-
-  def setKicadFile(kicadFile: String): Unit = {
-    this.kicadFile = kicadFile
-  }
-
-  override def mouseWheelMoved(mouseWheelEvent: MouseWheelEvent): Unit = {
-    println(mouseWheelEvent.getWheelRotation, "scrolliosis")
-  }
-
-}
-
 class RefinementsPanel extends JPanel {
   private val tree = new TreeTable(new RefinementsTreeTableModel(edgrpc.Refinements()))
   tree.setShowColumns(true)
