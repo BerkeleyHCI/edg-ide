fe76a65ccb97dff5e69264b78167ccf2a9773e43
nikhilJain17 <nikhil.jain0987@gmail.com>
2021-05-08 11:32:33 -0700

    split extractPos into two functions -- extractPosition and getSublistByName

--- src/main/scala/edg_ide/ui/KicadParser.scala
+++ src/main/scala/edg_ide/ui/KicadParser.scala
@@ -1,5 +1,7 @@
 package edg_ide.ui
 import java.io.{File, FileNotFoundException}
+import java.nio.charset.MalformedInputException
+
 import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 import scala.io.Source
 
@@ -108,59 +110,64 @@ class KicadParser(kicadFilePath:String) {
     }
   }
 
+  // Given an SList, scan the SList for a sublist tagged by `name`
+  // i.e. in the list ((a b c) ((d) e f) (d 1 2) d h i), if we search for
+  // sublists tagged by 'd', we want to return (d 1 2)
+  def getSublistByName(list: SList, name: String): List[SList] = {
+    list.values
+      .filter {
+        case _:Atom => false
+        case SList(values) =>
+          values.head match {
+            case atom: Atom =>
+              atom.symbol.equals(name)
+            case _: SList =>false
+          }
+      }
+      .map {
+        // all Atoms should have been filtered out at this point
+        e => e.asInstanceOf[SList]
+      }
+  }
 
-  // Given an Element, determine if it contains a valid position. If so, return the position. Otherwise, return None.
-  // (tag xpos ypos) where xpos and ypos are numerical values and tag is a string, such as "start", "end", "at"
-  def extractPos(element: Element, tag:String): Option[(Float, Float)] = {
-    var position = (0f, 0f)
-    element match {
-      case Atom(symbol) =>
-        None
-      case SList(values) =>
-        if (values.length != 3) return None
-
-        // Verify (tag ....)
-        values.head match {
-          case Atom(symbol) =>
-            if (!symbol.equalsIgnoreCase(tag)) {
-              return None
-            }
-          case SList(values) =>
-            return None
-
-        }
-
-        // Verify (... xpos ...) is numerical value
-        values.tail.head match {
-          case Atom(symbol) =>
-            val parsedFloat = symbol.toFloatOption
-            if (parsedFloat.isEmpty) {
-              return None
-            }
-            else {
-              position = (parsedFloat.get, position._2)
-            }
-          case SList(values) =>
-            return None
-        }
-
-        // Verify (... ypos) is numerical value
-        values.tail.tail.head match {
-          case Atom(symbol) =>
-            val parsedFloat = symbol.toFloatOption
-            if (parsedFloat.isEmpty) {
-              return None
-            }
-            else {
-              position = (position._1, parsedFloat.get)
-            }
-
-          case SList(values) =>
-            return None
-        }
-
-        Some(position)
+  // Given a position-identifying list of the form (name:String, a:Float, b:Float),
+  // return (a, b) or throw an Exception
+  def extractPosition(list:SList): (Float, Float) = {
+    if (list.values.length != 3 || !list.values.head.isInstanceOf[Atom]) {
+      throw new IllegalArgumentException("Expected list of the form (string float float) but got: " + list.toString)
     }
+
+    var position = (0.0f, 0.0f)
+
+    // Verify (... xpos ...) is numerical value
+    list.values.tail.head match {
+      case Atom(symbol) =>
+        val parsedFloat = symbol.toFloatOption
+        if (parsedFloat.isEmpty) {
+          throw new IllegalArgumentException("Expected float for first value, but got non-numerical value: " + parsedFloat.toString)
+        }
+        else {
+          position = (parsedFloat.get, position._2)
+        }
+      case SList(values) =>
+        throw new IllegalArgumentException("Expected float for first value, but got SList: " + values.toString)
+    }
+
+    // Verify (... ypos) is numerical value
+    list.values.tail.tail.head match {
+      case Atom(symbol) =>
+        val parsedFloat = symbol.toFloatOption
+        if (parsedFloat.isEmpty) {
+          throw new IllegalArgumentException("Expected float for first value, but got non-numerical value: " + parsedFloat.toString)
+        }
+        else {
+          position = (position._1, parsedFloat.get)
+        }
+      case SList(values) =>
+        throw new IllegalArgumentException("Expected float for first value, but got SList: " + values.toString)
+    }
+
+    position
   }
 
   def parseKicadFile(): ArrayBuffer[KicadComponent] = {
@@ -181,44 +188,41 @@ class KicadParser(kicadFilePath:String) {
           // We can safely assume parsedElement is an SList because of FSilkLine.
           // Ensure that startPosList and endPosList are well-formed, and get start/end positions.
 
-          val startPosList = parsedElement.asInstanceOf[SList].values
-          .map { e => extractPos(e, "start")} // mutate all Elements to be either Some((x, y)) or None }
-          .filter(coordinates => coordinates.isDefined) // drop the Nones
-          .map(coordinates => coordinates.getOrElse((-1f,-1f))) // unwrap the Optional (could just use get)
+          val startPosList = getSublistByName(parsedElement.asInstanceOf[SList], "start")
+          val endPosList = getSublistByName(parsedElement.asInstanceOf[SList], "end")
+          try {
+            if (startPosList.length == 1 && endPosList.length == 1) {
+              val (startX, startY) = extractPosition(startPosList(0))
+              val (endX, endY) = extractPosition(endPosList(0))
 
-          val endPosList = parsedElement.asInstanceOf[SList].values
-          .map { e => extractPos(e, "end") }
-          .filter(optionalCoordinates => optionalCoordinates.isDefined)
-          .map(coordinates => coordinates.getOrElse((-1f,-1f)))
-
-          if (startPosList.length == 1 && endPosList.length == 1) {
-            val startPos = startPosList(0)
-            val endPos = endPosList(0)
-            kicadComponents.addOne(new Line(startPos._1, startPos._2, endPos._1, endPos._2))
+              kicadComponents.addOne(new Line(startX, startY, endX, endY))
+            }
           }
+          catch {
+            // extractPosition() throws an illegal argument exception if the inputted list is malformed
+            case e:IllegalArgumentException => e.printStackTrace()
+          }
+
         }
 
         else if (isRectPad(parsedElement)) {
           // We can safely assume parsedElement is an SList because of FSilkLine.
           // Ensure that locationList and sizeList are well-formed, and get location/size.
-          val locationList = parsedElement.asInstanceOf[SList].values
-            .map {
-              e => extractPos(e, "at") // mutate all Elements to be either Some((x, y)) or None
+
+          val locationList = getSublistByName(parsedElement.asInstanceOf[SList], "at")
+          val sizeList = getSublistByName(parsedElement.asInstanceOf[SList], "size")
+          try {
+            if (locationList.length == 1 && sizeList.length == 1) {
+              val (x, y) = extractPosition(locationList(0))
+              val (w, h) = extractPosition(sizeList(0))
+
+              val name = parsedElement.asInstanceOf[SList].values(1).asInstanceOf[Atom].symbol
+              kicadComponents.addOne(new Rectangle(x, y, w, h, name))
             }
-            .filter(coordinates => coordinates.isDefined) // drop the Nones
-            .map(coordinates => coordinates.getOrElse((-1f,-1f))) // unwrap the Optional (could just use get)
-
-          val sizeList = parsedElement.asInstanceOf[SList].values
-            .map { e => extractPos(e, "size") }
-            .filter(optionalCoordinates => optionalCoordinates.isDefined)
-            .map(coordinates => coordinates.getOrElse((-1f,-1f)))
-
-          if (locationList.length == 1 && sizeList.length == 1) {
-            val location = locationList(0)
-            val size = sizeList(0)
-
-            val name = parsedElement.asInstanceOf[SList].values(1).asInstanceOf[Atom].symbol
-            kicadComponents.addOne(new Rectangle(location._1, location._2, size._1, size._2, name))
+          }
+          catch {
+            // extractPosition() throws an illegal argument exception if the inputted list is malformed
+            case e: IllegalArgumentException => e.printStackTrace()
           }
         }
       }
@@ -227,14 +231,15 @@ class KicadParser(kicadFilePath:String) {
       kicadComponents
     }
     catch {
-      case e:FileNotFoundException =>
+      // Fail noisily but don't crash the plugin -- just don't draw anything
+      case e:FileNotFoundException => {}
         println("Couldn't open kicad file for parsing: ", kicadFilePath)
         e.printStackTrace()
-        throw e
+        ArrayBuffer()
       case t:Throwable =>
         println("Error while parsing kicad file")
         t.printStackTrace()
-        throw t
+        ArrayBuffer()
     }
   }
 
