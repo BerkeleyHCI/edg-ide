804b1fd3ddb4aa503dd25eec68e7f36531ea15bc
nikhilJain17 <nikhil.jain0987@gmail.com>
2021-05-29 02:15:07 -0400

    more cleanups

--- src/main/scala/edg_ide/ui/KicadParser.scala
+++ src/main/scala/edg_ide/ui/KicadParser.scala
@@ -21,58 +21,6 @@ class KicadParser(kicadFilePath:String) {
     this.kicadFile = kicadFile
   }
 
-  // Given a parsed Element, determine if it is a "fp_line"
-  // e.g. (fp_line (start x y) (end x y) (layer F.SilkS) (width w))
-  def isFpLine(element: Element):Boolean = {
-
-    element match {
-      case Atom(symbol) =>
-        false
-      case SList(values) =>
-        // Match on "fp_line" atom in list
-        val lineFiltered = values.filter {
-          case Atom(symbol) => symbol.equalsIgnoreCase("fp_line")
-          case _:SList => false
-        }
-
-        lineFiltered.nonEmpty
-    }
-  }
-
-  // Given an element, determine if it is an "fp_line" with a "FSilk.S" layer
-  // e.g. (fp_line (start x y) (end 22.x y) (layer F.SilkS) (width w))
-  def isFSilkLine(element: Element): Boolean = {
-    if (!isFpLine(element)) return false
-
-    element match {
-      case Atom(symbol) =>
-        // should never get here, since isFpLine() would reject any Atoms
-        throw new Exception("[Error] Got an Atom but accepted it as an fp_line...something is wrong!: " + element.toString)
-        false
-      case SList(values) =>
-        // Search for "Silk" layer
-        // (layer F.SilkS)
-
-        val layerFiltered = values.tail.filter {
-          case _:Atom =>
-            false
-          case s:SList =>
-            if (s.values.length != 2) false
-            else {
-              val bothAtoms = s.values.head.isInstanceOf[Atom] && s.values.tail.head.isInstanceOf[Atom]
-              if (!bothAtoms) false
-              else {
-                // Can safely convert to atom
-                val layerTokenAtom = s.values.head.asInstanceOf[Atom]
-                val silkTokenAtom = s.values.tail.head.asInstanceOf[Atom]
-                layerTokenAtom.symbol.equalsIgnoreCase("layer") && silkTokenAtom.symbol.contains("Silk")
-              }
-            }
-        }
-
-        layerFiltered.nonEmpty
-    }
-  }
 
   def containsAtom(slist:SList, atom: String): Boolean = {
     val filtered = slist.values.filter {
@@ -81,42 +29,6 @@ class KicadParser(kicadFilePath:String) {
     }
 
     filtered.nonEmpty
-  }
-
-
-  // Given a parsed Element, determine if it is a "pad"
-  // (pad ... (at x y) (size w h) ... )
-  def isPad(element: Element):Boolean = {
-    element match {
-      case Atom(symbol) =>
-        false
-      case SList(values) =>
-        // Match on "pad" atom in list
-        val padFiltered = values.filter {
-          case Atom(symbol) => symbol.equalsIgnoreCase("pad")
-          case _:SList => false
-
-        }
-        padFiltered.nonEmpty
-    }
-  }
-
-  // Given a parsed Element, determine if it is a rectangular pad
-  // (pad ... (at x y) (size w h) ... )
-  def isRectPad(element: Element): Boolean = {
-    if (!isPad(element)) return false
-
-    element match {
-      case Atom(symbol) =>
-        false
-      case SList(values) =>
-        // Match on "rect" (or roundrect) atom in list
-        val rectFiltered = values.filter {
-          case Atom(symbol) => symbol.equalsIgnoreCase("rect")
-          case _:SList => false
-        }
-        rectFiltered.nonEmpty
-    }
   }
 
   // Given an SList, scan the SList for the only sublist tagged by `name`
@@ -154,37 +66,16 @@ class KicadParser(kicadFilePath:String) {
     require((list.values.length == 3) && (list.values.head.isInstanceOf[Atom]),
       "Expected list of the form (string float float) but got: " + list.toString)
 
-    var position = (0.0f, 0.0f)
 
-    // Verify (... xpos ...) is numerical value
-    list.values.tail.head match {
-      case Atom(symbol) =>
-        val parsedFloat = symbol.toFloatOption
-        if (parsedFloat.isEmpty) {
-          throw new IllegalArgumentException("Expected float for first value, but got non-numerical value: " + parsedFloat.toString)
+    (list.values.tail.head, list.values.tail.tail.head) match {
+      case (Atom(xpos), Atom(ypos)) =>
+        (xpos.toFloatOption, ypos.toFloatOption) match {
+          case (Some(xpos), Some(ypos)) => (xpos, ypos)
+          case _ => throw new IllegalArgumentException("Expected (float, float), but got non-numerical value: " + xpos + ypos)
         }
-        else {
-          position = (parsedFloat.get, position._2)
-        }
-      case SList(values) =>
-        throw new IllegalArgumentException("Expected float for first value, but got SList: " + values.toString)
+      case badVal => throw new IllegalArgumentException("Expected float (float, float), but got: " + badVal)
     }
 
-    // Verify (... ypos) is numerical value
-    list.values.tail.tail.head match {
-      case Atom(symbol) =>
-        val parsedFloat = symbol.toFloatOption
-        if (parsedFloat.isEmpty) {
-          throw new IllegalArgumentException("Expected float for first value, but got non-numerical value: " + parsedFloat.toString)
-        }
-        else {
-          position = (position._1, parsedFloat.get)
-        }
-      case SList(values) =>
-        throw new IllegalArgumentException("Expected float for first value, but got SList: " + values.toString)
-    }
-
-    position
   }
 
   def parseKicadFile(): ArrayBuffer[KicadComponent] = {
@@ -202,10 +93,8 @@ class KicadParser(kicadFilePath:String) {
       for (parsedElement <- parsed.values) {
 
         parsedElement match {
-          case _: Atom =>
           case list: SList =>
             if (list.values.nonEmpty && list.values.head.isInstanceOf[Atom]) {
-
               val layerList = getOnlySublistByName(list, "layer")
               list.values.head.asInstanceOf[Atom].symbol match {
 
@@ -214,28 +103,29 @@ class KicadParser(kicadFilePath:String) {
                   val locationList = getOnlySublistByName(parsedElement.asInstanceOf[SList], "at")
                   val sizeList = getOnlySublistByName(parsedElement.asInstanceOf[SList], "size")
 
-                  if (locationList.isDefined && sizeList.isDefined) {
-                    val (x, y) = extractPosition(locationList.get)
-                    val (w, h) = extractPosition(sizeList.get)
+                  require(locationList.isDefined && sizeList.isDefined)
+                  val (x, y) = extractPosition(locationList.get)
+                  val (w, h) = extractPosition(sizeList.get)
 
-                    val name = parsedElement.asInstanceOf[SList].values(1).asInstanceOf[Atom].symbol
-                    kicadComponents.addOne(new Rectangle(x, y, w, h, name))
-                  }
+                  val name = parsedElement.asInstanceOf[SList].values(1).asInstanceOf[Atom].symbol
+                  kicadComponents.addOne(new Rectangle(x, y, w, h, name))
+
 
                 case "fp_line" if getOnlySublistByName(list, "layer").isDefined && layerList.get.values.length == 2 && layerList.get.values.tail.head == Atom("F.SilkS") =>
-                  val startPosList = getOnlySublistByName(parsedElement.asInstanceOf[SList], "start")
-                  val endPosList = getOnlySublistByName(parsedElement.asInstanceOf[SList], "end")
+                  val startPosList = getOnlySublistByName(list, "start")
+                  val endPosList = getOnlySublistByName(list, "end")
 
-                  if (startPosList.isDefined && endPosList.isDefined) {
-                    val (startX, startY) = extractPosition(startPosList.get)
-                    val (endX, endY) = extractPosition(endPosList.get)
+                  require(startPosList.isDefined && endPosList.isDefined)
+                  val (startX, startY) = extractPosition(startPosList.get)
+                  val (endX, endY) = extractPosition(endPosList.get)
 
-                    kicadComponents.addOne(new Line(startX, startY, endX, endY))
-                  }
+                  kicadComponents.addOne(new Line(startX, startY, endX, endY))
 
                 case _ =>
             }
           }
+
+          case _ =>
         }
 
       }
