5ef6bc0e5b65dd229cd156254b50a1792ecf398c
nikhilJain17 <nikhil.jain0987@gmail.com>
2021-05-31 18:45:44 -0400

    error handling ofr getOnlySublistByName and rewrote loop in functional style

--- src/main/scala/edg_ide/ui/KicadParser.scala
+++ src/main/scala/edg_ide/ui/KicadParser.scala
@@ -34,7 +34,7 @@ class KicadParser(kicadFilePath:String) {
   // Given an SList, scan the SList for the only sublist tagged by `name`
   // i.e. in the list ((a b c) ((d) e f) (d 1 2) d h i), if we search for
   // sublists tagged by 'd', we want to return (d 1 2)
-  def getOnlySublistByName(list: SList, name: String): Option[SList] = {
+  def getOnlySublistByName(list: SList, name: String): SList = {
     val subLists = list.values
       .filter {
         case _:Atom => false
@@ -51,34 +51,40 @@ class KicadParser(kicadFilePath:String) {
       }
 
     if (subLists.length == 1)
-      return Some(subLists(0))
-
-    if (subLists.isEmpty)
-      return None
+      return subLists(0)
 
     // enforcing the "Only" in getOnlySublistByName
-    throw new IllegalArgumentException("Multiple subLists with name, only expected one: " + name)
+    if (subLists.length > 1)
+      throw new IllegalArgumentException("Multiple subLists with name, only expected one: " + name)
+
+    if (subLists.isEmpty)
+      throw new IllegalArgumentException("No subList with name, expected one: " + name)
+
+    SList(List[Element]())
+
   }
 
   // Given a position-identifying list of the form (name:String, a:Float, b:Float),
   // return (a, b) or throw an Exception
   def extractPosition(list:SList): (Float, Float) = {
-    require((list.values.length == 3) && (list.values.head.isInstanceOf[Atom]),
-      "Expected list of the form (string float float) but got: " + list.toString)
 
-
-    (list.values.tail.head, list.values.tail.tail.head) match {
-      case (Atom(xpos), Atom(ypos)) =>
-        (xpos.toFloatOption, ypos.toFloatOption) match {
-          case (Some(xpos), Some(ypos)) => (xpos, ypos)
-          case _ => throw new IllegalArgumentException("Expected (float, float), but got non-numerical value: " + xpos + ypos)
+    list.values match {
+      case (_:Atom) :: (b:Atom) :: (c:Atom) :: Nil =>
+        (b, c) match {
+          case (Atom(xPos), Atom(yPos)) =>
+            (xPos.toFloatOption, yPos.toFloatOption) match {
+              case (Some(xPos), Some(yPos)) => (xPos, yPos)
+              case _ => throw new IllegalArgumentException("Expected (float, float), but got non-numerical value: " + xPos + yPos)
+            }
+          case badVal => throw new IllegalArgumentException("Expected (float, float), but got: " + badVal)
         }
-      case badVal => throw new IllegalArgumentException("Expected float (float, float), but got: " + badVal)
+
+      case badVal => throw new IllegalArgumentException("Expected (float, float), but got: " + badVal)
     }
 
   }
 
-  def parseKicadFile(): ArrayBuffer[KicadComponent] = {
+  def parseKicadFile(): List[KicadComponent] = {
     try {
       val fileReader = Source.fromFile(kicadFile)
       val lines = fileReader.getLines()
@@ -88,46 +94,39 @@ class KicadParser(kicadFilePath:String) {
       }
 
       val parsed = ExpressionParser.parse(s)
-      var kicadComponents = new ArrayBuffer[KicadComponent]
 
-      for (parsedElement <- parsed.values) {
+      val kicadComponents = parsed.values.flatMap {
+        case list: SList if list.values.nonEmpty && list.values.head.isInstanceOf[Atom] =>
+            list.values.head.asInstanceOf[Atom].symbol match {
 
-        parsedElement match {
-          case list: SList =>
-            if (list.values.nonEmpty && list.values.head.isInstanceOf[Atom]) {
-              val layerList = getOnlySublistByName(list, "layer")
-              list.values.head.asInstanceOf[Atom].symbol match {
+              // Match on the different components we want to handle
+              case "pad" if containsAtom(list, "rect") || containsAtom(list, "roundrect") =>
+                val locationList = getOnlySublistByName(list, "at")
+                val sizeList = getOnlySublistByName(list, "size")
 
-                // Match on the different components we want to handle
-                case "pad" if containsAtom(list, "rect") || containsAtom(list, "roundrect") =>
-                  val locationList = getOnlySublistByName(parsedElement.asInstanceOf[SList], "at")
-                  val sizeList = getOnlySublistByName(parsedElement.asInstanceOf[SList], "size")
+                val (x, y) = extractPosition(locationList)
+                val (w, h) = extractPosition(sizeList)
 
-                  require(locationList.isDefined && sizeList.isDefined)
-                  val (x, y) = extractPosition(locationList.get)
-                  val (w, h) = extractPosition(sizeList.get)
+                val name = list.values(1).asInstanceOf[Atom].symbol
+                Some(Rectangle(x, y, w, h, name))
 
-                  val name = parsedElement.asInstanceOf[SList].values(1).asInstanceOf[Atom].symbol
-                  kicadComponents.addOne(new Rectangle(x, y, w, h, name))
-
-
-                case "fp_line" if getOnlySublistByName(list, "layer").isDefined && layerList.get.values.length == 2 && layerList.get.values.tail.head == Atom("F.SilkS") =>
+              case "fp_line" =>
+                val layerList = getOnlySublistByName(list, "layer")
+                if (layerList.values.length == 2 && layerList.values.tail.head == Atom("F.SilkS")) {
                   val startPosList = getOnlySublistByName(list, "start")
                   val endPosList = getOnlySublistByName(list, "end")
 
-                  require(startPosList.isDefined && endPosList.isDefined)
-                  val (startX, startY) = extractPosition(startPosList.get)
-                  val (endX, endY) = extractPosition(endPosList.get)
+                  val (startX, startY) = extractPosition(startPosList)
+                  val (endX, endY) = extractPosition(endPosList)
+                  Some(Line(startX, startY, endX, endY))
+                }
+                else
+                  None
 
-                  kicadComponents.addOne(new Line(startX, startY, endX, endY))
-
-                case _ =>
-            }
+              case _ => None
           }
 
-          case _ =>
-        }
-
+        case _ => None
       }
 
       fileReader.close()
@@ -138,17 +137,13 @@ class KicadParser(kicadFilePath:String) {
       case e:FileNotFoundException => {}
         println("Couldn't open kicad file for parsing: ", kicadFilePath)
         e.printStackTrace()
-        ArrayBuffer()
+        List()
       case t:Throwable =>
         println("Error while parsing kicad file")
         t.printStackTrace()
-        ArrayBuffer()
+        List()
     }
   }
-
-
-
-
 }
 
 
