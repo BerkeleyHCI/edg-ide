582c8bf497bcdcd380af60c05aeb91946932aefe
Richard Lin <elpato25@gmail.com>
2022-08-01 23:39:00 -0700

    Stream forwarding to console

--- src/main/scala/edg_ide/runner/DesignTopRunConfigurationType.scala
+++ src/main/scala/edg_ide/runner/DesignTopRunConfigurationType.scala
@@ -17,12 +17,19 @@ import com.intellij.openapi.ui.LabeledComponent
 import com.intellij.openapi.util.JDOMExternalizerUtil
 import com.intellij.psi.search.ExecutionSearchScopes
 import com.jetbrains.python.run.PythonTracebackFilter
+import edg.EdgirUtils.SimpleLibraryPath
 import edg.ElemBuilder
-import edg.compiler.{DesignStructuralValidate, PythonInterface}
+import edg.compiler.{DesignStructuralValidate, ExprValue, PythonInterface}
+import edg.util.Errorable
 import edg_ide.ui.{BlockVisualizerService, EdgCompilerService}
+import edgir.elem.elem
+import edgir.ref.ref
+import edgir.schema.schema
+import edgrpc.hdl.{hdl => edgrpc}
+
 import org.jdom.Element
 
-import java.io.{OutputStream, PrintWriter, StringWriter}
+import java.io.{InputStream, OutputStream, PrintWriter, StringWriter}
 import java.net.URI
 import java.nio.file.Paths
 import javax.swing.{Icon, JComponent, JPanel, JTextField}
@@ -108,33 +115,83 @@ class DesignTopRunConfiguration(project: Project, factory: ConfigurationFactory,
         ProgressManager.getInstance().run(new Task.Backgroundable(project, "EDG compiling") {
           override def run(indicator: ProgressIndicator): Unit = {
             processHandler.startNotify()
+            console.print(s"Starting compilation of ${options.designName}\n", ConsoleViewContentType.SYSTEM_OUTPUT)
+
+            // This structure is quite nasty, but is needed to give a stream handle in case something crashes,
+            // in which case pythonInterface is not a valid reference
+            var outputStream: Option[InputStream] = None
+            var errorStream: Option[InputStream] = None
+            def forwardProcessOutput(): Unit = {
+              outputStream.foreach { outputStream =>
+                while (outputStream.available() > 0) {
+                  console.print(outputStream.read().toChar.toString, ConsoleViewContentType.SYSTEM_OUTPUT)
+                }
+              }
+              errorStream.foreach { errorStream =>
+                while (errorStream.available() > 0) {
+                  console.print(errorStream.read().toChar.toString, ConsoleViewContentType.ERROR_OUTPUT)
+                }
+              }
+            }
 
             try {
-              EdgCompilerService(project).pyLib.withPythonInterface(
-                new PythonInterface(Paths.get(project.getBasePath).resolve("HdlInterfaceService.py").toFile)) {
+              val pythonInterface = new PythonInterface(
+                Paths.get(project.getBasePath).resolve("HdlInterfaceService.py").toFile) {
 
+                override def onLibraryRequest(element: ref.LibraryPath): Unit = {
+                  console.print(s"Compile ${element.toSimpleString}\n", ConsoleViewContentType.USER_INPUT)
+                }
+                override def onLibraryRequestComplete(element: ref.LibraryPath,
+                                                      result: Errorable[(schema.Library.NS.Val, Option[edgrpc.Refinements])]): Unit = {
+                  forwardProcessOutput()
+                  result match {
+                    case Errorable.Error(msg) => console.print(msg, ConsoleViewContentType.ERROR_OUTPUT)
+                    case _ =>
+                  }
+                }
+                override def onElaborateGeneratorRequest(element: ref.LibraryPath, values: Map[ref.LocalPath, ExprValue]): Unit = {
+                  console.print(s"Generate ${element.toSimpleString}\n", ConsoleViewContentType.USER_INPUT)
+                }
+                override def onElaborateGeneratorRequestComplete(element: ref.LibraryPath,
+                                                                 values: Map[ref.LocalPath, ExprValue],
+                                                                 result: Errorable[elem.HierarchyBlock]): Unit = {
+                  forwardProcessOutput()
+                  result match {
+                    case Errorable.Error(msg) => console.print(msg, ConsoleViewContentType.ERROR_OUTPUT)
+                    case _ =>
+                  }
+                }
+              }
+              outputStream = Some(pythonInterface.processOutputStream)
+              errorStream = Some(pythonInterface.processErrorStream)
+
+              val (compiled, compiler, refinements, reloadTime, compileTime) = EdgCompilerService(project).pyLib
+                  .withPythonInterface(pythonInterface) {
                 indicator.setText("EDG compiling")
 
                 val designType = ElemBuilder.LibraryPath(options.designName)
                 val designModule = options.designName.split('.').init.mkString(".")
-                val (compiled, compiler, refinements, reloadTime, compileTime) = EdgCompilerService(project)
-                    .compile(designModule, designType, Some(indicator))
-
-                indicator.setText("EDG compiling: validating")
-                val checker = new DesignStructuralValidate()
-                val errors = compiler.getErrors() ++ checker.map(compiled)
-                console.print(s"Compiled (reload: $reloadTime ms, compile: $compileTime ms)",
-                  ConsoleViewContentType.SYSTEM_OUTPUT)
-                if (errors.nonEmpty) {
-                  console.print(s"Compiled design has ${errors.length} errors", ConsoleViewContentType.ERROR_OUTPUT)
-                }
-
-                BlockVisualizerService(project).setDesignTop(compiled, compiler, refinements, errors)
-                BlockVisualizerService(project).setLibrary(EdgCompilerService(project).pyLib)
+                EdgCompilerService(project).compile(designModule, designType, Some(indicator))
               }
+              pythonInterface.shutdown()
+
+              forwardProcessOutput()  // dump remaining process output (shouldn't happen)
+
+              indicator.setText("EDG compiling: validating")
+              val checker = new DesignStructuralValidate()
+              val errors = compiler.getErrors() ++ checker.map(compiled)
+              console.print(s"Compiled (reload: $reloadTime ms, compile: $compileTime ms)\n",
+                ConsoleViewContentType.SYSTEM_OUTPUT)
+              if (errors.nonEmpty) {
+                console.print(s"Compiled design has ${errors.length} errors\n", ConsoleViewContentType.ERROR_OUTPUT)
+              }
+
+              BlockVisualizerService(project).setDesignTop(compiled, compiler, refinements, errors)
+              BlockVisualizerService(project).setLibrary(EdgCompilerService(project).pyLib)
             } catch {
               case e: Throwable =>
-                console.print(s"Compiler internal error: ${e.toString}", ConsoleViewContentType.ERROR_OUTPUT)
+                forwardProcessOutput()  // dump remaining process output first
+                console.print(s"Compiler internal error: ${e.toString}\n", ConsoleViewContentType.ERROR_OUTPUT)
                 val stackWriter = new StringWriter()
                 e.printStackTrace(new PrintWriter(stackWriter))
                 console.print(stackWriter.toString, ConsoleViewContentType.ERROR_OUTPUT)
