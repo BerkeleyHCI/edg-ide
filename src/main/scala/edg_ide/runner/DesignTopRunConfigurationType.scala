40fc7054e3d92a376d4f5d1eeebc94af4eba540f
Richard Lin <elpato25@gmail.com>
2022-08-14 17:05:27 -0700

    Update DesignTopRunConfigurationType.scala

--- src/main/scala/edg_ide/runner/DesignTopRunConfigurationType.scala
+++ src/main/scala/edg_ide/runner/DesignTopRunConfigurationType.scala
@@ -49,7 +49,6 @@ class DesignTopConfigurationFactory(confType: ConfigurationType) extends Configu
 class DesignTopRunConfigurationOptions extends RunConfigurationOptions {
   var designName: String = ""
   var netlistFile: String = ""
-  var buildAllLibraries: Boolean = true
 }
 
 
