3b07b6ad8a404577c3ea0a9cc7ed6fed948955f3
Richard Lin <elpato25@gmail.com>
2022-11-23 14:33:58 -0800

    clean up unit test

--- src/test/scala/edg_ide/dse/tests/DseParameterSearchTest.scala
+++ src/test/scala/edg_ide/dse/tests/DseParameterSearchTest.scala
@@ -1,6 +1,6 @@
 package edg_ide.dse.tests
 
-import edg.compiler.{BooleanValue, FloatValue, IntValue, RangeValue, TextValue}
+import edg.compiler.{BooleanValue, ExprValue, FloatValue, IntValue, RangeValue, TextValue}
 import edg.util.Errorable
 import edg.wir.DesignPath
 import edg_ide.dse.DseParameterSearch
@@ -11,93 +11,90 @@ import org.scalatest.matchers.should.Matchers
 class DseParameterSearchTest extends AnyFlatSpec with Matchers {
   behavior of "DseParameterSearch"
 
-  it should "parse String to BooleanValue" in {
-    val reference = DseParameterSearch(DesignPath(), Seq(BooleanValue(true)))
-    reference.valuesStringToConfig("true, false").get.getValues.map(_._1) should equal(
-      Seq(BooleanValue(true), BooleanValue(false)))
-    reference.valuesStringToConfig("false, true").get.getValues.map(_._1) should equal(
-      Seq(BooleanValue(false), BooleanValue(true)))
-    reference.valuesStringToConfig("true").get.getValues.map(_._1) should equal(
-      Seq(BooleanValue(true)))
+  protected def shouldRoundtrip(values: Seq[ExprValue]): Unit = {
+    val parameter = DseParameterSearch(DesignPath(), values)
+    parameter.valuesStringToConfig(parameter.valuesToString()).get.getValues.map(_._1) should equal(values)
+  }
 
-    reference.valuesStringToConfig("") shouldBe a[Errorable.Error]
-    reference.valuesStringToConfig(",") shouldBe a[Errorable.Error]
-    reference.valuesStringToConfig("ducks") shouldBe a[Errorable.Error]
+  protected def parseString(examples: Seq[ExprValue], str: String): Errorable[Seq[ExprValue]] = {
+    val exampleParameter = DseParameterSearch(DesignPath(), examples)
+    exampleParameter.valuesStringToConfig(str).map(_.getValues).map(_.map(_._1))
+  }
+
+  it should "parse String to BooleanValue" in {
+    val reference = Seq(BooleanValue(true))
+    parseString(reference, "true, false").get should equal(Seq(BooleanValue(true), BooleanValue(false)))
+    parseString(reference, "false, true").get should equal(Seq(BooleanValue(false), BooleanValue(true)))
+    parseString(reference, "true").get should equal(Seq(BooleanValue(true)))
+
+    parseString(reference, "") shouldBe a[Errorable.Error]
+    parseString(reference, ",") shouldBe a[Errorable.Error]
+    parseString(reference, "ducks") shouldBe a[Errorable.Error]
+  }
+
+  it should "roundtrip BooleanValue" in {
+    shouldRoundtrip(Seq(BooleanValue(true)))
+    shouldRoundtrip(Seq(BooleanValue(false)))
+    shouldRoundtrip(Seq(BooleanValue(true), BooleanValue(false)))
+    shouldRoundtrip(Seq(BooleanValue(false), BooleanValue(true)))
   }
 
   it should "parse String to IntValue" in {
-    val reference = DseParameterSearch(DesignPath(), Seq(IntValue(1)))
-    reference.valuesStringToConfig("1").get.getValues.map(_._1) should equal(
-      Seq(IntValue(1)))
-    reference.valuesStringToConfig("1, 2").get.getValues.map(_._1) should equal(
-      Seq(IntValue(1), IntValue(2)))
-    reference.valuesStringToConfig("1, 2,3").get.getValues.map(_._1) should equal(
-      Seq(IntValue(1), IntValue(2), IntValue(3)))
+    val reference = Seq(IntValue(1))
+    parseString(reference, "1").get should equal(Seq(IntValue(1)))
+    parseString(reference, "1, 2").get should equal(Seq(IntValue(1), IntValue(2)))
+    parseString(reference, "1, 2,3").get should equal(Seq(IntValue(1), IntValue(2), IntValue(3)))
 
-    reference.valuesStringToConfig("") shouldBe a[Errorable.Error]
-    reference.valuesStringToConfig(",") shouldBe a[Errorable.Error]
-    reference.valuesStringToConfig("1.0") shouldBe a[Errorable.Error]
-    reference.valuesStringToConfig("ducks") shouldBe a[Errorable.Error]
+    parseString(reference, "") shouldBe a[Errorable.Error]
+    parseString(reference, ",") shouldBe a[Errorable.Error]
+    parseString(reference, "1.0") shouldBe a[Errorable.Error]
+    parseString(reference, "ducks") shouldBe a[Errorable.Error]
   }
 
   it should "parse String to FloatValue" in {
-    val reference = DseParameterSearch(DesignPath(), Seq(FloatValue(1.0)))
-    reference.valuesStringToConfig("1.0").get.getValues.map(_._1) should equal(
-      Seq(FloatValue(1.0)))
-    reference.valuesStringToConfig("1.0, 2.0").get.getValues.map(_._1) should equal(
-      Seq(FloatValue(1.0), FloatValue(2.0)))
-    reference.valuesStringToConfig("1.0, 2.0,3.0").get.getValues.map(_._1) should equal(
-      Seq(FloatValue(1.0), FloatValue(2.0), FloatValue(3.0)))
+    val reference = Seq(FloatValue(1.0))
+    parseString(reference, "1.0").get should equal(Seq(FloatValue(1.0)))
+    parseString(reference, "1.0, 2.0").get should equal(Seq(FloatValue(1.0), FloatValue(2.0)))
+    parseString(reference, "1.0, 2.0,3.0").get should equal(Seq(FloatValue(1.0), FloatValue(2.0), FloatValue(3.0)))
 
-    reference.valuesStringToConfig("") shouldBe a[Errorable.Error]
-    reference.valuesStringToConfig(",") shouldBe a[Errorable.Error]
-    reference.valuesStringToConfig("ducks") shouldBe a[Errorable.Error]
+    parseString(reference, "") shouldBe a[Errorable.Error]
+    parseString(reference, ",") shouldBe a[Errorable.Error]
+    parseString(reference, "ducks") shouldBe a[Errorable.Error]
   }
 
   it should "parse String to TextValue" in {
-    val reference = DseParameterSearch(DesignPath(), Seq(TextValue("")))
-    reference.valuesStringToConfig("").get.getValues.map(_._1) should equal(
-      Seq(TextValue("")))
-    reference.valuesStringToConfig("abc").get.getValues.map(_._1) should equal(
-      Seq(TextValue("abc")))
-    reference.valuesStringToConfig(",def").get.getValues.map(_._1) should equal(
-      Seq(TextValue(""), TextValue("def")))
-    reference.valuesStringToConfig("abc,def").get.getValues.map(_._1) should equal(
-      Seq(TextValue("abc"), TextValue("def")))
-    reference.valuesStringToConfig("abc, def").get.getValues.map(_._1) should equal(
-      Seq(TextValue("abc"), TextValue(" def")))
-    reference.valuesStringToConfig("abc,\" def\"").get.getValues.map(_._1) should equal(
-      Seq(TextValue("abc"), TextValue(" def")))
-    reference.valuesStringToConfig("abc,\" def, ghi\"").get.getValues.map(_._1) should equal(
-      Seq(TextValue("abc"), TextValue(" def, ghi")))
-    reference.valuesStringToConfig("abc,\\\" def, ghi\\\"").get.getValues.map(_._1) should equal(
+    val reference = Seq(TextValue(""))
+    parseString(reference, "").get should equal(Seq(TextValue("")))
+    parseString(reference, "abc").get should equal(Seq(TextValue("abc")))
+    parseString(reference, ",def").get should equal(Seq(TextValue(""), TextValue("def")))
+    parseString(reference, "abc,def").get should equal(Seq(TextValue("abc"), TextValue("def")))
+    parseString(reference, "abc, def").get should equal(Seq(TextValue("abc"), TextValue(" def")))
+    parseString(reference, "abc,\" def\"").get should equal(Seq(TextValue("abc"), TextValue(" def")))
+    parseString(reference, "abc,\" def, ghi\"").get should equal(Seq(TextValue("abc"), TextValue(" def, ghi")))
+    parseString(reference, "abc,\\\" def, ghi\\\"").get should equal(
       Seq(TextValue("abc"), TextValue("\" def"), TextValue(" ghi\"")))
-    reference.valuesStringToConfig("a\\\\bc").get.getValues.map(_._1) should equal(
-      Seq(TextValue("a\\bc")))
+    parseString(reference, "a\\\\bc").get should equal(Seq(TextValue("a\\bc")))
 
-    reference.valuesStringToConfig("abc\"") shouldBe a[Errorable.Error]
-    reference.valuesStringToConfig("abc,\"") shouldBe a[Errorable.Error]
-    reference.valuesStringToConfig("abc,\"de") shouldBe a[Errorable.Error]
-    reference.valuesStringToConfig("abc,de\"de\"") shouldBe a[Errorable.Error]
-    reference.valuesStringToConfig("abc,de\\") shouldBe a[Errorable.Error]
+    parseString(reference, "abc\"") shouldBe a[Errorable.Error]
+    parseString(reference, "abc,\"") shouldBe a[Errorable.Error]
+    parseString(reference, "abc,\"de") shouldBe a[Errorable.Error]
+    parseString(reference, "abc,de\"de\"") shouldBe a[Errorable.Error]
+    parseString(reference, "abc,de\\") shouldBe a[Errorable.Error]
   }
 
   it should "parse String to RangeValue" in {
-    val reference = DseParameterSearch(DesignPath(), Seq(RangeValue(-1, 1)))
-    reference.valuesStringToConfig("(1, 2)").get.getValues.map(_._1) should equal(
-      Seq(RangeValue(1, 2)))
-    reference.valuesStringToConfig("(-1, 1)").get.getValues.map(_._1) should equal(
-      Seq(RangeValue(-1, 1)))
-    reference.valuesStringToConfig("(-2.5, 4.2)").get.getValues.map(_._1) should equal(
-      Seq(RangeValue(-2.5, 4.2)))
-    reference.valuesStringToConfig("(-1, 1), (-2.5, 4.2)").get.getValues.map(_._1) should equal(
+    val reference = Seq(RangeValue(-1, 1))
+    parseString(reference, "(1, 2)").get should equal(Seq(RangeValue(1, 2)))
+    parseString(reference, "(-1, 1)").get should equal(Seq(RangeValue(-1, 1)))
+    parseString(reference, "(-2.5, 4.2)").get should equal(Seq(RangeValue(-2.5, 4.2)))
+    parseString(reference, "(-1, 1), (-2.5, 4.2)").get should equal(
       Seq(RangeValue(-1, 1), RangeValue(-2.5, 4.2)))
-    reference.valuesStringToConfig("(-1, 1), (-2.5, 4.2),(2,3)").get.getValues.map(_._1) should equal(
+    parseString(reference, "(-1, 1), (-2.5, 4.2),(2,3)").get should equal(
       Seq(RangeValue(-1, 1), RangeValue(-2.5, 4.2), RangeValue(2, 3)))
 
-    reference.valuesStringToConfig("") shouldBe a[Errorable.Error]
-    reference.valuesStringToConfig("(-1, 1, 1)") shouldBe a[Errorable.Error]
-    reference.valuesStringToConfig("(-1)") shouldBe a[Errorable.Error]
-    reference.valuesStringToConfig("(-1), (-1, 2)") shouldBe a[Errorable.Error]
+    parseString(reference, "") shouldBe a[Errorable.Error]
+    parseString(reference, "(-1, 1, 1)") shouldBe a[Errorable.Error]
+    parseString(reference, "(-1)") shouldBe a[Errorable.Error]
+    parseString(reference, "(-1), (-1, 2)") shouldBe a[Errorable.Error]
   }
 }
