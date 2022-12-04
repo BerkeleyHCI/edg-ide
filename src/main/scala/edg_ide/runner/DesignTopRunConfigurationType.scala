625518c84dd9806beed9df16cc34790532e013dc
Pierce Chong <48197758+PierceCCH@users.noreply.github.com>
2022-12-03 16:58:05 -0800

    plumb toggles from DesignTopConfig to NetlistGenerator (#50)
    Added toggle buttons to select between "refdes" and "pathName" modes.
    
    Added arguments variable which is a Map[String, String] to pass value of
    selection to runBackend function.
    
    Co-authored-by: Richard Lin <richard.lin@berkeley.edu>

--- src/main/scala/edg_ide/runner/DesignTopRunConfigurationType.scala
+++ src/main/scala/edg_ide/runner/DesignTopRunConfigurationType.scala
@@ -10,9 +10,10 @@ import com.intellij.openapi.options.SettingsEditor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.JDOMExternalizerUtil
 import com.intellij.psi.search.ExecutionSearchScopes
-import com.intellij.ui.components.JBLabel
+import com.intellij.ui.components.{JBLabel, JBRadioButton}
 import com.intellij.util.ui.FormBuilder
 import com.jetbrains.python.run.PythonTracebackFilter
+import edgir.init.init.ValInit.Val.Boolean
 import org.jdom.Element
 
 import java.awt.GridLayout
@@ -47,13 +48,23 @@ class DesignTopConfigurationFactory(confType: ConfigurationType) extends Configu
   override def getOptionsClass: Class[DesignTopRunConfigurationOptions] = classOf[DesignTopRunConfigurationOptions]
 }
 
+object RefdesMode extends Enumeration {
+  type selections = Value
+
+  val refdes = Value(0, "refdes")
+  val pathName = Value(1, "pathName")
+
+  def toEnum(s: String): Option[Value] = {
+    values.find(_.toString == s)
+  }
+}
 
 class DesignTopRunConfigurationOptions extends RunConfigurationOptions {
   var designName: String = ""
   var netlistFile: String = ""
   var pdfFile: String = ""
+  var toggle: RefdesMode.selections = RefdesMode.refdes
 }
-
 
 class DesignTopRunConfiguration(project: Project, factory: ConfigurationFactory, name: String)
     extends RunConfigurationBase[DesignTopRunConfigurationOptions](project, factory, name) {
@@ -61,6 +72,7 @@ class DesignTopRunConfiguration(project: Project, factory: ConfigurationFactory,
 
   override def getConfigurationEditor: SettingsEditor[_ <: RunConfiguration] = new DesignTopSettingsEditor(project)
 
+  // This is new
   override def getState(executor: Executor, environment: ExecutionEnvironment): RunProfileState = {
     new RunProfileState {
       override def execute(executor: Executor, runner: ProgramRunner[_]): ExecutionResult = {
@@ -82,28 +94,40 @@ class DesignTopRunConfiguration(project: Project, factory: ConfigurationFactory,
   val kFieldDesignName = "DESIGN_NAME"
   val kFieldNetlistName = "NETLIST_NAME"
   val kPdfFileName = "PDF_NAME"
+  val kFieldRefdesMode = "REFDESMODE_NAME"
+
+  // Allows persistence of run configuration
   override def readExternal(element: Element): Unit = {
     super.readExternal(element)
     options.designName = JDOMExternalizerUtil.readField(element, kFieldDesignName, "")
     options.netlistFile = JDOMExternalizerUtil.readField(element, kFieldNetlistName, "")
     options.pdfFile = JDOMExternalizerUtil.readField(element, kPdfFileName, "")
+    RefdesMode.toEnum(JDOMExternalizerUtil.readField(element, kFieldRefdesMode)).foreach(options.toggle = _)
   }
+
   override def writeExternal(element: Element): Unit = {
     super.writeExternal(element)
     JDOMExternalizerUtil.writeField(element, kFieldDesignName, options.designName)
     JDOMExternalizerUtil.writeField(element, kFieldNetlistName, options.netlistFile)
     JDOMExternalizerUtil.writeField(element, kPdfFileName, options.pdfFile)
+    JDOMExternalizerUtil.writeField(element, kFieldRefdesMode, options.toggle.toString)
   }
 }
-
 
 class DesignTopSettingsEditor(project: Project) extends SettingsEditor[DesignTopRunConfiguration] {
   protected val designName = new JTextField()
   protected val netlistFile = new JTextField()  // no browse button b/c FileChooser can't create new files
   protected val pdfFile = new JTextField()
+  protected val toggleRefdes = new JBRadioButton()
+  protected val togglePathname = new JBRadioButton()
+  protected val toggleButtons = new ButtonGroup()
+  toggleButtons.add(toggleRefdes)
+  toggleButtons.add(togglePathname)
 
   protected val panel = FormBuilder.createFormBuilder()
       .addLabeledComponent(new JBLabel("Design top name"), designName, false)
+      .addLabeledComponent(new JBLabel("Select Netlist Refdes value"), toggleRefdes)
+      .addLabeledComponent(new JBLabel("Select Netlist Path Name"), togglePathname)
       .addLabeledComponent(new JBLabel("Netlist output file"), netlistFile, false)
       .addLabeledComponent(new JBLabel("PDF output file"), pdfFile, false)
       .addComponentFillVertically(new JPanel(), 0)
@@ -113,12 +137,28 @@ class DesignTopSettingsEditor(project: Project) extends SettingsEditor[DesignTop
     designName.setText(s.options.designName)
     netlistFile.setText(s.options.netlistFile)
     pdfFile.setText(s.options.pdfFile)
+
+    s.options.toggle match {
+      case RefdesMode.refdes =>
+        toggleRefdes.setSelected(true)
+        togglePathname.setSelected(false)
+      case RefdesMode.pathName =>
+        toggleRefdes.setSelected(false)
+        togglePathname.setSelected(true)
+    }
   }
 
   override def applyEditorTo(s: DesignTopRunConfiguration): Unit = {
     s.options.designName = designName.getText
     s.options.netlistFile = netlistFile.getText
     s.options.pdfFile = pdfFile.getText
+    if (toggleRefdes.isSelected){
+      s.options.toggle = RefdesMode.refdes
+    } else if (togglePathname.isSelected) {
+      s.options.toggle = RefdesMode.pathName
+    } else {
+      // Ignore invalid user selection
+    }
   }
 
   override def createEditor(): JComponent = panel
