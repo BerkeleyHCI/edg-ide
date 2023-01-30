ee949c542706568cee021ff97469686c1c5a30ae
Richard Lin <elpato25@gmail.com>
2023-01-29 16:36:56 -0800

    ticks

--- src/main/scala/edg_ide/swing/dse/JScatterPlot.scala
+++ src/main/scala/edg_ide/swing/dse/JScatterPlot.scala
@@ -1,8 +1,11 @@
 package edg_ide.swing.dse
 
+import edg_ide.swing.DrawAnchored
+
 import java.awt.event.{MouseEvent, MouseMotionAdapter}
 import java.awt.{Dimension, Graphics, Rectangle}
 import javax.swing.{JComponent, Scrollable}
+import scala.collection.mutable
 
 
 /** Scatterplot widget with two numerical axes, with labels inside the plot.
@@ -13,13 +16,16 @@ import javax.swing.{JComponent, Scrollable}
 class JScatterPlot[ValueType] extends JComponent with Scrollable{
   private var data: Seq[(Float, Float, ValueType)] = Seq()
 
+  private val kPlotMarginPx = 4 // px margin on every side of the plot
+
   private val kPointSizePx = 4 // diameter in px
   private val kSnapDistancePx = 4 // distance (box) to snap for a click
 
-  private val kPlotMarginPx = 4 // px margin on every side of the plot
+  private val kMinTickSpacingPx = 32  // min spacing between axis ticks, used to determine tick resolution
+  private val kTickSizePx = 4
 
   private var xOrigin = 0  // zero data is here in screen coordinates
-  private var xScale = 1.0f  // multiply data coord by this to get screen pos
+  private var xScale = 1.0f  // in px/data; multiply data coord by this to get screen pos
   private var yOrigin = 0
   private var yScale = -1.0f  // screen coordinates are +Y = down
 
@@ -34,12 +40,41 @@ class JScatterPlot[ValueType] extends JComponent with Scrollable{
     repaint()
   }
 
+  // Returns all the axis ticks given some scale, screen origin, screen size, and min screen spacing
+  private def getAxisTicks(scale: Float, screenOrigin: Int, screenSize: Int, minScreenSpacing: Int): Seq[Float] = {
+    val minDataSpacing = math.abs(minScreenSpacing / scale)  // min tick spacing in data units
+    val tickSpacing = math.pow(10, math.log10(minDataSpacing).ceil)  // actual spacing in data units
+    val tickBound1 = -screenOrigin / scale  // because scale could be negative this could be the high end
+    val tickBound2 = (screenSize - screenOrigin) / scale
+
+    var tickPos = (math.floor(math.min(tickBound1, tickBound2) / tickSpacing) * tickSpacing).toFloat
+    val tickEnd = math.max(tickBound1, tickBound2)
+    val ticksBuilder = mutable.ArrayBuffer[Float]()
+    while (tickPos <= tickEnd) {
+      ticksBuilder.append(tickPos)
+      tickPos = (tickPos + tickSpacing).toFloat
+    }
+    ticksBuilder.toSeq
+  }
+
   private def paintAxes(paintGraphics: Graphics): Unit = {
     // bottom horizontal axis
     paintGraphics.drawLine(0, getHeight-1, getWidth-1, getHeight-1)
+    getAxisTicks(xScale, xOrigin, getWidth, kMinTickSpacingPx).foreach { tickPos =>
+      val screenX = (tickPos * xScale).toInt + xOrigin
+      paintGraphics.drawLine(screenX, getHeight - 1, screenX, getHeight - 1 - kTickSizePx)
+      DrawAnchored.drawLabel(paintGraphics, f"$tickPos%.02g",
+        (screenX, getHeight - 1 - kTickSizePx), DrawAnchored.Bottom)
+    }
 
     // left vertical axis
     paintGraphics.drawLine(0, 0, 0, getHeight-1)
+    getAxisTicks(yScale, yOrigin, getHeight, kMinTickSpacingPx).foreach { tickPos =>
+      val screenY = (tickPos * yScale).toInt + yOrigin
+      paintGraphics.drawLine(0, screenY, kTickSizePx, screenY)
+      DrawAnchored.drawLabel(paintGraphics, f"$tickPos%.02g",
+        (kTickSizePx, screenY), DrawAnchored.Left)
+    }
   }
 
   private def paintData(paintGraphics: Graphics): Unit = {
