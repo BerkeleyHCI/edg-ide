bd3e693a7fe782df80f955b8185458e9b96a2ee5
Pierce Chong <48197758+PierceCCH@users.noreply.github.com>
2023-02-02 15:27:02 -0800

    cleanup code

--- src/main/scala/edg_ide/swing/ExprVarToValue.scala
+++ src/main/scala/edg_ide/swing/ExprVarToValue.scala
@@ -1,28 +1,18 @@
 package edg.compiler
-
 import edg.wir.DesignPath
 import edgir.expr.expr
 import edgir.ref.ref
 
 object ExprVarToValue {
-
   def apply(item: expr.ValueExpr, compiler: Compiler, designPath: DesignPath): String = new ExprVarToValue(compiler, designPath).map(item)
 }
 
 class ExprVarToValue(compiler: Compiler, designPath: DesignPath) extends ExprToString() {
-
-/* TODO:
-  Override mapRef to show key and current values compared with constraints
- */
-
   override def mapRef(path: ref.LocalPath): String = {
     val value = compiler.getParamValue((designPath ++ path).asIndirect) match {
       case Some(value) => value.toStringValue
       case None => "Unsolved"
     }
-
-    s"Value of ${(designPath ++ path).asIndirect} (${value})"
+    s"Value of ${(designPath ++ path).asIndirect} ${value}"
   }
-
-
 }
