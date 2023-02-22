a098879ee16d23b330363f495ffbd412bb3b2267
Richard Lin <elpato25@gmail.com>
2023-02-21 22:17:12 -0800

    basic test

--- src/test/scala/edg_ide/proven/tests/ProvenDataReaderTest.scala
+++ src/test/scala/edg_ide/proven/tests/ProvenDataReaderTest.scala
@@ -7,6 +7,7 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 import java.io.File
+import scala.collection.SeqMap
 
 
 class ProvenDataReaderTest extends AnyFlatSpec with Matchers {
@@ -15,12 +16,13 @@ class ProvenDataReaderTest extends AnyFlatSpec with Matchers {
   it should "read in data" in {
     val data = ProvenDataReader.read(new File("src/main/resources/proven-designs/data.csv"))
     data.getRecords(ElemBuilder.LibraryPath("electronics_lib.Microcontroller_Stm32f103.Stm32f103_48"))
-        .map(record => (record._1.file.getName, record._1.version)) shouldEqual(
+        .map(record => (record._1.file.getName, record._1.version)) shouldEqual
         Seq(("TofArrayTest.edg", "c5a263037c00c5585586453d10de8963fa39d0e7"),
           ("BldcDriverBoard.edg", "de2f2692524fa4f20c05d237879fca5222511790"))
-    )
 
-    println(data.data.toSeq.sortBy(_._2.length).map(pair => f"${pair._1.toFullString} (${pair._2.length})"))
-    println(data.getByDesign(ElemBuilder.LibraryPath("electronics_lib.JlcCapacitor.JlcCapacitor")).mapValues(_.length).toSeq)
+    data.getByDesign(ElemBuilder.LibraryPath("electronics_lib.Microcontroller_Stm32f103.Stm32f103_48"))
+        .map { case (design, records) => (design._1.getName, design._2) -> records.length } shouldEqual
+        SeqMap(("TofArrayTest.edg", "c5a263037c00c5585586453d10de8963fa39d0e7") -> 1,
+          ("BldcDriverBoard.edg", "de2f2692524fa4f20c05d237879fca5222511790") -> 1)
   }
 }
