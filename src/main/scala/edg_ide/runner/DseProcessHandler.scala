a045abbb380819d0c93c1c9df44434f6b777d732
Richard Lin <richard.lin@berkeley.edu>
2023-04-27 12:25:55 +0200

    Python packaging compatibility (#108)
    - Update to be compatible when the HDL is installed from a pip package,
    instead of editing the HDL repo directly.

--- src/main/scala/edg_ide/runner/DseProcessHandler.scala
+++ src/main/scala/edg_ide/runner/DseProcessHandler.scala
@@ -14,7 +14,7 @@ import edg_ide.ui.{BlockVisualizerService, DseService, EdgCompilerService}
 import edgir.schema.schema
 import edgir.schema.schema.Design
 
-import java.io.{FileWriter, OutputStream, PrintWriter, StringWriter}
+import java.io.{File, FileWriter, OutputStream, PrintWriter, StringWriter}
 import java.nio.file.{Files, Paths}
 import scala.collection.{SeqMap, mutable}
 import scala.jdk.CollectionConverters.IterableHasAsJava
@@ -151,14 +151,18 @@ class DseProcessHandler(project: Project, options: DseRunConfigurationOptions, v
     }
 
     try {
-      val (pythonCommand, sdkName) = CompileProcessHandler.getPythonInterpreter(project, options.designName).mapErr(
-        msg => s"while getting Python interpreter path: $msg"
-      ).get
+      val (pythonCommand, pythonPaths, sdkName) = CompileProcessHandler.getPythonInterpreter(project, options.designName)
+        .mapErr(
+          msg => s"while getting Python interpreter path: $msg"
+        ).get
       console.print(s"Using interpreter from configured SDK '$sdkName': $pythonCommand\n",
         ConsoleViewContentType.LOG_INFO_OUTPUT)
 
-      val pythonInterface = new LoggingPythonInterface(
-        Paths.get(project.getBasePath).resolve("HdlInterfaceService.py").toFile, pythonCommand, console)
+      val hdlServerOption = PythonInterface.serverFileOption(Some(Paths.get(project.getBasePath).toFile))
+      hdlServerOption.foreach { _ =>
+        console.print(s"Using local HDL server\n", ConsoleViewContentType.LOG_INFO_OUTPUT)
+      }
+      val pythonInterface = new LoggingPythonInterface(hdlServerOption, pythonPaths, pythonCommand, console)
       pythonInterfaceOption = Some(pythonInterface)
 
       EdgCompilerService(project).pyLib.withPythonInterface(pythonInterface) {
