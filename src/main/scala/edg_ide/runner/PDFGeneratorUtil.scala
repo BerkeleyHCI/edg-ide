7c953dfc410066ac15b27e88f8a15eacf73e255d
TayKaiJun <tehohno@gmail.com>
2023-05-17 16:45:34 -0700

    Update PDFGeneratorUtil.scala
    Hierarchical path in table now shows each level of the design path as a clickable link to that block

--- src/main/scala/edg_ide/runner/PDFGeneratorUtil.scala
+++ src/main/scala/edg_ide/runner/PDFGeneratorUtil.scala
@@ -24,9 +24,10 @@ object PDFGeneratorUtil{
       page size is large enough/page margin is small enough so that header/footer content do not exceed
       the available space in the document.
    */
-  final val PAGE_MARGIN = 16f
+  private final val PAGE_MARGIN = 16f
+  private final val TABLE_ROW_HEIGHT = 23f
   final val font = new Font(Font.HELVETICA, 12f, Font.UNDERLINE, Color.BLUE)
-  final val TABLE_ROW_HEIGHT = 23f
+  final val font_bold = new Font(Font.HELVETICA, 12f, Font.BOLD, Color.BLACK)
 
 
   private def generatePageSize(node: ElkNode): (Float, Float) = {
@@ -35,10 +36,12 @@ object PDFGeneratorUtil{
     (width, height)
   }
 
-
+  /*
+    Generates a map of component class name to a set of DesignPath that uses the component.
+    This is used to generate a table of components that are used multiple times in the design.
+   */
   def getDuplicationList(block: HierarchyBlock, path: DesignPath = DesignPath(),
-                         dupList: mutable.Map[LibraryPath, Set[DesignPath]]): Unit ={
-
+                          dupList: mutable.Map[LibraryPath, Set[DesignPath]] = mutable.Map()): Map[LibraryPath, Set[DesignPath]] = {
     val className = block.getSelfClass
     dupList.getOrElseUpdate(className, Set(path)) match {
       case existingSet: Set[DesignPath] => dupList(className) = existingSet + path
@@ -47,10 +50,12 @@ object PDFGeneratorUtil{
     block.blocks.asPairs.map {
       case (name, subblock) => (name, subblock.`type`)
     }.collect {
-      case (name, BlockLike.Type.Hierarchy(subblock)) =>  (path + name, subblock)
+      case (name, BlockLike.Type.Hierarchy(subblock)) => (path + name, subblock)
     }.foreach {
       case (path, subblock) => getDuplicationList(subblock, path, dupList)
     }
+
+    dupList.toMap
   }
 
 
@@ -58,142 +63,123 @@ object PDFGeneratorUtil{
                mappers: Seq[PropertyMapper[NodeDataWrapper, PortWrapper, EdgeWrapper]] = Seq(),
                fileName: String): Unit = {
 
-    val dupList = mutable.Map[LibraryPath, Set[DesignPath]]()
-    getDuplicationList(content, dupList = dupList)
+    val dupList = getDuplicationList(content)
 
-    try {
-      val document = new Document()
-      val writer = PdfWriter.getInstance(document, new FileOutputStream(fileName))
+    val document = new Document()
+    val writer = PdfWriter.getInstance(document, new FileOutputStream(fileName))
 
 
-      def printNode(node: ElkNode, className: LibraryPath, path: DesignPath): Unit = {
-        val dupSet = dupList.getOrElse(className, Set.empty)
-        val (width, height) = generatePageSize(node)
-        val adjustedHeight = if(dupSet.size == 1) {
-          height
-        } else {
-          TABLE_ROW_HEIGHT * dupSet.size + height + ElkNodePainter.margin
-        }
-        document.setPageSize(new Rectangle(width, adjustedHeight))
-        document.setMargins(2*PAGE_MARGIN, 2*PAGE_MARGIN, PAGE_MARGIN, PAGE_MARGIN)
+    def printNode(node: ElkNode, className: LibraryPath, path: DesignPath): Unit = {
+      val dupSet = dupList.getOrElse(className, Set.empty)
+      val (width, height) = generatePageSize(node)
+      val adjustedHeight = if(dupSet.size == 1) {
+        height
+      } else {
+        TABLE_ROW_HEIGHT * dupSet.size + height + ElkNodePainter.margin
+      }
+      document.setPageSize(new Rectangle(width, adjustedHeight))
+      document.setMargins(2*PAGE_MARGIN, 2*PAGE_MARGIN, PAGE_MARGIN, PAGE_MARGIN)
 
-        // Sets the header and footer of each page. Target of the component target link is set as the header.
-        val targetAnchor = new Anchor(path.toString)
-        targetAnchor.setName(path.toString)
-        val headerPhrase = new Phrase
-        headerPhrase.add(targetAnchor)
-        val pageHeader = new HeaderFooter(headerPhrase,false)
-        pageHeader.setAlignment(Element.ALIGN_LEFT)
-        document.setHeader(pageHeader)
+      // Sets the header and footer of each page. Target of the component target link is set as the header.
+      val targetAnchor = new Anchor(path.toString, font_bold)
+      targetAnchor.setName(path.toString)
+      val headerPhrase = new Phrase
+      headerPhrase.add(targetAnchor)
+      val pageHeader = new HeaderFooter(headerPhrase,false)
+      pageHeader.setAlignment(Element.ALIGN_LEFT)
+      document.setHeader(pageHeader)
 
-        val pageFooter = new HeaderFooter(new Phrase("Page "), true)
-        pageFooter.setAlignment(Element.ALIGN_RIGHT)
-        pageFooter.setBorder(Rectangle.NO_BORDER)
-        document.setFooter(pageFooter)
+      val pageFooter = new HeaderFooter(new Phrase("Page "), true)
+      pageFooter.setAlignment(Element.ALIGN_RIGHT)
+      pageFooter.setBorder(Rectangle.NO_BORDER)
+      document.setFooter(pageFooter)
 
-        /*
-        Metadata for the Footer does not align the page number correctly if
-        document.open() is called before document.setPageSize() was executed
-         */
-        if (!document.isOpen) {
-          document.open()
-        } else {
-          document.newPage
-        }
+      /*
+      Metadata for the Footer does not align the page number correctly if
+      document.open() is called before document.setPageSize() was executed
+       */
+      if (!document.isOpen) {
+        document.open()
+      } else {
+        document.newPage
+      }
 
-        val cb = writer.getDirectContent
-        val graphics = cb.createGraphics(width, adjustedHeight)
-        val painter = new ElkNodePainter(node)
-        painter.paintComponent(graphics, Color.white)
-        graphics.dispose()
+      val cb = writer.getDirectContent
+      val graphics = cb.createGraphics(width, adjustedHeight)
+      val painter = new ElkNodePainter(node)
+      painter.paintComponent(graphics, Color.white)
+      graphics.dispose()
 
-        // Prints out table if there are multiple usages of the same component
-        if(dupSet.size > 1) {
-          val table = new PdfPTable(1)
+      // Prints out table if there are multiple usages of the same component
+      if(dupSet.size > 1) {
+        val table = new PdfPTable(1)
 
-          val message = new Paragraph(s"${className.toSimpleString} is also used at the following:")
-          val title = new PdfPCell(message)
-          title.setBorder(Rectangle.NO_BORDER)
-          table.addCell(title)
+        val message = new Paragraph(s"${className.toSimpleString} is also used at the following:")
+        val title = new PdfPCell(message)
+        title.setBorder(Rectangle.NO_BORDER)
+        table.addCell(title)
 
-          dupSet.foreach { path =>
-            val cellContent = new Paragraph
-
-            val (parent, current) = path.split
-            if (parent.toString == "(root)") {
-              val parentAnchor = new Anchor
-              parentAnchor.setReference(s"#${parent.toString}")
-              parentAnchor.add(new Phrase(s"${parent.toString}", font))
-              parentAnchor.add(new Phrase(s".${current.toString}"))
-              cellContent.add(parentAnchor)
-              println("root is the only parent")
-            } else {
-              var prevString = ""
-              path.toString.split('.').foreach { string =>
-                val anchor = new Anchor
-
-                if(prevString == "") {
-                  anchor.setReference(s"#${string}")
-                  anchor.add(new Phrase(s"${string}", font))
-                } else {
-                  anchor.setReference(s"#${prevString}.${string}")
-                  anchor.add(new Phrase(s".${string}", font))
+        dupSet.foreach { dupPath =>
+          val cellContent = new Paragraph
+          val (parent, current) = dupPath.split
+          if (parent.toString == "(root)") {
+            val parentAnchor = new Anchor
+            parentAnchor.setReference(s"#${parent.toString}")
+            parentAnchor.add(new Phrase(s"${parent.toString}", font))
+            parentAnchor.add(new Phrase(s".${current}"))
+            cellContent.add(parentAnchor)
+          } else {
+            var prevStep = ""
+            dupPath.toString.split('.').foreach { step =>
+              val anchor = new Anchor
+              if (prevStep == "") {
+                anchor.setReference(s"#${step}")
+                anchor.add(new Phrase(s"${step}", font))
+                prevStep = step
+              } else {
+                if (step == dupPath.toString.split('.').last) {
+                  anchor.add(new Phrase(s".${step}"))
                 }
-                cellContent.add(anchor)
-                prevString = prevString + "." + string
+                else {
+                  anchor.setReference(s"#${prevStep}.${step}")
+                  anchor.add(new Phrase("."))
+                  anchor.add(new Phrase(s"${step}", font))
+                  prevStep = prevStep + "." + step
+                }
               }
-            }
-
-//            var prevString = "(root)"
-//            path.toString.split('.').foreach { string =>
-//              val anchor = new Anchor
-//              anchor.setReference(s"#${prevString}.${string}")
-//              println(s"#${prevString}.${string}")
-//              anchor.add(new Phrase(s".${string}", font))
-//              prevString = prevString + "." + string
-//              cellContent.add(anchor)
-//            }
-//            val parentAnchor = new Anchor
-//            val (parent, current) = path.split
-//            parentAnchor.setReference(s"#${parent.toString}")
-//            parentAnchor.add(new Phrase(s"${parent.toString}", font))
-//            parentAnchor.add(new Phrase(s".${current.toString}"))
-//
-//            cellContent.add(parentAnchor)
-            val cell = new PdfPCell(cellContent)
-            cell.setFixedHeight(TABLE_ROW_HEIGHT)
-            table.addCell(cell)
-          }
-          val tableY = adjustedHeight - height + ElkNodePainter.margin
-          table.setTotalWidth(width - 2 * ElkNodePainter.margin)
-          table.writeSelectedRows(0, -1, ElkNodePainter.margin.toFloat, tableY, writer.getDirectContent())
-        }
-      }
-
-
-      def printNextHierarchyLevel(block: HierarchyBlock, path: DesignPath = DesignPath()): Unit = {
-        val node = HierarchyGraphElk.HBlockToElkNode(block, path, mappers = mappers)
-        printNode(node, block.getSelfClass, path)
-
-        block.blocks.asPairs.map {
-          case (name, subblock) => (name, subblock.`type`)
-        }.collect {
-          case (name, BlockLike.Type.Hierarchy(subblock)) if subblock.blocks.nonEmpty => (path + name, subblock, subblock.getSelfClass)
-        }.foreach {
-          case (path, subblock, className) => {
-            if (dupList.getOrElse(className, Set.empty).headOption.contains(path)) {
-              printNextHierarchyLevel(subblock, path)
+              cellContent.add(anchor)
             }
           }
+          val cell = new PdfPCell(cellContent)
+          cell.setFixedHeight(TABLE_ROW_HEIGHT)
+          table.addCell(cell)
         }
-      }
 
-      printNextHierarchyLevel(content)
-      document.close()
-    } catch {
-      case e: FileNotFoundException => println(s"Couldn't find ${fileName}.")
-      case e: SecurityException => println(s"Access to ${fileName} denied.")
-      case e: IOException => println(s"Had an IOException trying to read ${fileName}")
+        val tableY = adjustedHeight - height + ElkNodePainter.margin
+        table.setTotalWidth(width - 2 * ElkNodePainter.margin)
+        table.writeSelectedRows(0, -1, ElkNodePainter.margin.toFloat, tableY, writer.getDirectContent())
+      }
     }
+
+
+    def printNextHierarchyLevel(block: HierarchyBlock, path: DesignPath = DesignPath()): Unit = {
+      val node = HierarchyGraphElk.HBlockToElkNode(block, path, mappers = mappers)
+      printNode(node, block.getSelfClass, path)
+
+      block.blocks.asPairs.map {
+        case (name, subblock) => (name, subblock.`type`)
+      }.collect {
+        case (name, BlockLike.Type.Hierarchy(subblock)) if subblock.blocks.nonEmpty => (path + name, subblock, subblock.getSelfClass)
+      }.foreach {
+        case (path, subblock, className) => {
+          if (dupList.getOrElse(className, Set.empty).headOption.contains(path)) {
+            printNextHierarchyLevel(subblock, path)
+          }
+        }
+      }
+    }
+
+    printNextHierarchyLevel(content)
+    document.close()
   }
 }
