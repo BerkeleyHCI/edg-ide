3cd6c7b3d66e089b21a721446a44889a8013c0c0
Richard Lin <elpato25@gmail.com>
2023-05-19 21:49:48 -0700

    cleaning up and refactoring

--- src/main/scala/edg_ide/psi_edits/InsertBlockAction.scala
+++ src/main/scala/edg_ide/psi_edits/InsertBlockAction.scala
@@ -1,29 +1,14 @@
 package edg_ide.psi_edits
 
-import com.intellij.codeInsight.highlighting.HighlightManager
-import com.intellij.codeInsight.template.impl.{ConstantNode, TemplateState}
-import com.intellij.codeInsight.template.{Template, TemplateBuilderImpl, TemplateEditingAdapter, TemplateManager}
-import com.intellij.openapi.application.{ApplicationManager, ModalityState, ReadAction}
-import com.intellij.openapi.command.WriteCommandAction.writeCommandAction
-import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.editor.colors.EditorColors
-import com.intellij.openapi.editor.markup.RangeHighlighter
-import com.intellij.openapi.fileEditor.{FileEditorManager, OpenFileDescriptor, TextEditor}
+import com.intellij.codeInsight.template.TemplateManager
+import com.intellij.openapi.fileEditor.{FileEditorManager, TextEditor}
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.TextRange
-import com.intellij.psi.{PsiDocumentManager, PsiElement}
+import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
-import com.intellij.refactoring.rename.inplace.InplaceRefactoring
-import com.intellij.util.concurrency.AppExecutorUtil
 import com.jetbrains.python.psi._
 import edg.util.Errorable
 import edg_ide.util.ExceptionNotifyImplicits.{ExceptNotify, ExceptSeq}
-import edg_ide.util.{DesignAnalysisUtils, exceptable}
-
-import java.util
-import java.util.concurrent.Callable
-import scala.collection.convert.ImplicitConversions.`collection AsScalaIterable`
-import scala.collection.mutable
+import edg_ide.util.exceptable
 
 
 object InsertBlockAction {
@@ -31,53 +16,6 @@ object InsertBlockAction {
   val VALID_FUNCTION_NAMES = Seq("contents", "__init__")  // TODO support generators
   val VALID_SUPERCLASS = "edg_core.HierarchyBlock.Block"
 
-  private class TemplateListener(project: Project, editor: Editor, containingList: PyStatementList, newAssignIndex: Int, highlighters: Seq[RangeHighlighter]) extends TemplateEditingAdapter {
-    override def beforeTemplateFinished(state: TemplateState, template: Template): Unit = {
-      println(f"TemplateListener::beforeTemplateFinished")
-      super.beforeTemplateFinished(state, template)
-    }
-    override def templateFinished(template: Template, brokenOff: Boolean): Unit = {
-      println(f"TemplateListener::templateFinished($brokenOff)")
-      super.templateFinished(template, brokenOff)
-      if (brokenOff) {
-        templateAnyCancelled(template)
-      }
-      templateEnded(template)
-    }
-
-    override def currentVariableChanged(templateState: TemplateState, template: Template, oldIndex: Int, newIndex: Int): Unit = {
-      println(f"TemplateListener::currentVariableChanged($oldIndex, $newIndex)")
-      super.currentVariableChanged(templateState, template, oldIndex, newIndex)
-    }
-
-    override def waitingForInput(template: Template): Unit = {
-      println(f"TemplateListener::waitingForInput")
-      super.waitingForInput(template)
-    }
-
-    override def templateCancelled(template: Template): Unit = {
-      println(f"TemplateListener::templateCancelled")
-      super.templateCancelled(template)
-      templateAnyCancelled(template)
-      templateEnded(template)
-    }
-
-    // Called when the template is cancelled (broken off or "cancelled")
-    def templateAnyCancelled(template: Template): Unit = {
-      val assignCandidate = containingList.getStatements()(newAssignIndex)
-      require(assignCandidate.isInstanceOf[PyAssignmentStatement])
-      writeCommandAction(project).withName("TODO cancel").compute(() => {
-        assignCandidate.delete()
-      })
-    }
-
-    // Called when the template is ended for any reason (finished or cancelled)
-    def templateEnded(template: Template): Unit = {
-      highlighters.foreach { highlighter =>
-        HighlightManager.getInstance(project).removeSegmentHighlighter(editor, highlighter)
-      }
-    }
-  }
 
   /** Creates an action to insert a block of type libClass after some PSI element after.
     * Validation is performed before the action is generated, though the action itself may also return an error.
@@ -108,45 +46,10 @@ object InsertBlockAction {
     val assignAst = psiElementGenerator.createFromText(languageLevel,
       classOf[PyAssignmentStatement], s"$selfName.block = $selfName.Block(${libClass.getName}())")
 
-    def run: Unit = {
-      val newAssign = writeCommandAction(project).withName(actionName).compute(() => {
-        val newAssign = containingPsiList.addAfter(assignAst, after).asInstanceOf[PyAssignmentStatement]
-        val newAssignIndex = containingPsiList.getStatements.indexOf(newAssign)
-        // getReference is the self.thing
-        val assignName = newAssign.getTargets.head.asInstanceOf[PyTargetExpression].getReference
-
-        // these must be constructed before template creation, other template creation messes up the locations
-        val highlighters = new java.util.ArrayList[RangeHighlighter]()
-        HighlightManager.getInstance(project).addRangeHighlight(editor,
-          newAssign.getTextRange.getStartOffset, newAssign.getTextRange.getEndOffset,
-          EditorColors.LIVE_TEMPLATE_INACTIVE_SEGMENT, false, highlighters)
-
-//        PsiDocumentManager.getInstance(project).commitDocument(editor.getDocument)
-
-        val builder = new TemplateBuilderImpl(containingPsiList)
-        val assignText = newAssign.getText
-        //        val assignRange = TextRange.create(0, assignText.length)
-        builder.replaceElement(assignName, "kAssignName", new ConstantNode("quack"), true)
-        //    builder.setSelection(after)
-        builder.replaceElement(newAssign.getAssignedValue, "kAssignTarget", new ConstantNode(newAssign.getAssignedValue.getText), true)
-        builder.setEndVariableAfter(newAssign.getAssignedValue)
-
-        PsiDocumentManager.getInstance(project).doPostponedOperationsAndUnblockDocument(editor.getDocument)
-
-        val template = builder.buildInlineTemplate() // specifically must be an inline template (actually replace the PSI elements), otherwise the block of new code is inserted at the caret
-
-        new OpenFileDescriptor(project, after.getContainingFile.getVirtualFile, newAssign.getTextRange.getStartOffset)
-            .navigate(true) // sets focus on the text editor so the user can type into the template
-        editor.getCaretModel.moveToOffset(containingPsiList.getTextOffset)  // needed so the template is placed at the right location
-
-        manager.startTemplate(editor, template, new TemplateListener(project, editor, containingPsiList, newAssignIndex, highlighters.toSeq))
-
-        newAssign
-      })
-
-    }
-
-    () => run
+    () => new InsertionLiveTemplate[PyAssignmentStatement](project, editor, actionName, after, assignAst, Seq(
+      ("assign name", psi => psi.getTargets.head.asInstanceOf[PyTargetExpression], true),
+      ("assign target", psi => psi.getAssignedValue, false),
+    )).run()
 
 //    val containingPsiList = after.getParent
 //        .instanceOfExcept[PyStatementList](s"invalid position for insertion in ${after.getContainingFile.getName}")
---
+++ src/main/scala/edg_ide/psi_edits/LiveTemplateUtil.scala
@@ -0,0 +1,148 @@
+package edg_ide.psi_edits
+
+import com.intellij.codeInsight.highlighting.HighlightManager
+import com.intellij.codeInsight.template.impl.{ConstantNode, TemplateState}
+import com.intellij.codeInsight.template.{Template, TemplateBuilderImpl, TemplateEditingAdapter, TemplateManager}
+import com.intellij.openapi.command.WriteCommandAction.writeCommandAction
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.colors.EditorColors
+import com.intellij.openapi.editor.markup.RangeHighlighter
+import com.intellij.openapi.fileEditor.OpenFileDescriptor
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.popup.JBPopup
+import com.intellij.openapi.ui.{ComponentValidator, ValidationInfo}
+import com.intellij.psi.{PsiDocumentManager, PsiElement}
+import com.jetbrains.python.psi._
+
+import javax.swing.JEditorPane
+import scala.jdk.CollectionConverters.CollectionHasAsScala
+
+
+/** Utilities for insertion live templates.
+  *
+  * @param actionName name of the insertion action, for undo
+  * @param after insertion location, after this element
+  * @param newSubtree new subtree to be inserted
+  * @param variables list of variables for the live template, each specified as a name
+  *                  and function from the inserted subtree to the PSI element to be highlighted
+  *                  and boolean of whether it is a reference
+  */
+class InsertionLiveTemplate[TreeType <: PyStatement](project: Project, editor: Editor, actionName: String,
+                            after: PsiElement, newSubtree: TreeType,
+                            variables: Seq[(String, TreeType => PsiElement, Boolean)]) {
+  private class TemplateListener(project: Project, editor: Editor,
+                                 containingList: PyStatementList, newAssignIndex: Int,
+                                 tooltip: JBPopup, highlighters: Iterable[RangeHighlighter]) extends TemplateEditingAdapter {
+    override def beforeTemplateFinished(state: TemplateState, template: Template): Unit = {
+      super.beforeTemplateFinished(state, template)
+    }
+
+    override def templateFinished(template: Template, brokenOff: Boolean): Unit = {
+      // this is called when:
+      // - tab/enter-cycle through last element (brokenOff=false)
+      // - escape (brokenOff=true)
+      // this does NOT get called when making an edit outside the template (instead, templateCancelled is called)
+      super.templateFinished(template, brokenOff)
+      if (brokenOff) {
+        templateAnyCancelled(template)
+      }
+      templateEnded(template)
+    }
+
+    override def currentVariableChanged(templateState: TemplateState, template: Template, oldIndex: Int, newIndex: Int): Unit = {
+      // called when the selected template variable is changed (on tab/enter-cycling)
+      super.currentVariableChanged(templateState, template, oldIndex, newIndex)
+    }
+
+    override def waitingForInput(template: Template): Unit = {
+      // called when the template starts
+      super.waitingForInput(template)
+    }
+
+    override def templateCancelled(template: Template): Unit = {
+      // this is called only when making an edit outside the current templated item
+      // this does NOT get called when escaping during a template (instead, templateFinished is called with brokenOff=true)
+      println(f"TemplateListener::templateCancelled")
+      super.templateCancelled(template)
+      //      templateAnyCancelled(template)
+      templateEnded(template)
+    }
+
+    // Called when the template is cancelled (broken off or "cancelled")
+    def templateAnyCancelled(template: Template): Unit = {
+      val assignCandidate = containingList.getStatements()(newAssignIndex)
+      require(assignCandidate.isInstanceOf[PyAssignmentStatement])
+      writeCommandAction(project).withName("TODO cancel").compute(() => {
+        assignCandidate.delete()
+      })
+    }
+
+    // internal API, called when the template is ended for any reason (successful or not)
+    def templateEnded(template: Template): Unit = {
+      tooltip.closeOk(null)
+      highlighters.foreach { highlighter =>
+        HighlightManager.getInstance(project).removeSegmentHighlighter(editor, highlighter)
+      }
+    }
+  }
+
+  private def createTemplateTooltip(message: String, editor: Editor): JBPopup = {
+    var hintHeight: Int = 0
+
+    val validationInfo = new ValidationInfo(message, null).asWarning()
+    val popupBuilder = ComponentValidator.createPopupBuilder(
+      validationInfo,
+      (editorPane: JEditorPane) => {
+        hintHeight = editorPane.getPreferredSize.height
+      }
+    ).setCancelKeyEnabled(false)  // otherwise this eats the cancel keypress for the live template
+    val popup = popupBuilder.createPopup()
+    popup.showInBestPositionFor(editor)
+    popup
+  }
+
+  def run(): Unit = {
+    writeCommandAction(project).withName(actionName).compute(() => {
+      val containingList = after.getParent.asInstanceOf[PyStatementList]
+      val newStmt = containingList.addAfter(newSubtree, after).asInstanceOf[TreeType]
+      val newStmtIndex = containingList.getStatements.indexOf(newStmt)
+
+      // these must be constructed before template creation, other template creation messes up the locations
+      val highlighters = new java.util.ArrayList[RangeHighlighter]()
+      // flags = 0 means ignore esc, otherwise it eats the esc keypress
+      HighlightManager.getInstance(project).addOccurrenceHighlight(editor,
+        newStmt.getTextRange.getStartOffset, newStmt.getTextRange.getEndOffset,
+        EditorColors.LIVE_TEMPLATE_INACTIVE_SEGMENT, 0, highlighters)
+
+      new OpenFileDescriptor(project, containingList.getContainingFile.getVirtualFile, newStmt.getTextRange.getStartOffset)
+          .navigate(true) // sets focus on the text editor so the user can type into the template
+      editor.getCaretModel.moveToOffset(newStmt.getTextOffset) // needed so the template is placed at the right location
+
+      val tooltip = createTemplateTooltip("this is a tooltip", editor)
+
+      val builder = new TemplateBuilderImpl(newStmt)
+      val variablePsis = variables.map { case (variableName, variablePsiExtractor, isReference) =>
+        val variablePsi = variablePsiExtractor(newStmt)
+        if (!isReference) {
+          builder.replaceElement(variablePsi, variableName, new ConstantNode(variablePsi.getText), true)
+        } else {
+          builder.replaceElement(variablePsi.getReference, variableName, new ConstantNode(variablePsi.getReference.getCanonicalText), true)
+        }
+        variablePsi
+      }
+      variablePsis.lastOption.foreach { lastVariablePsi =>
+        builder.setEndVariableAfter(newStmt.getLastChild)
+      }
+
+      // must be called before building the template
+      PsiDocumentManager.getInstance(project).doPostponedOperationsAndUnblockDocument(editor.getDocument)
+
+      // specifically must be an inline template (actually replace the PSI elements), otherwise the block of new code is inserted at the caret
+      val template = builder.buildInlineTemplate()
+
+      TemplateManager.getInstance(project).startTemplate(editor, template, new TemplateListener(
+        project, editor, containingList, newStmtIndex,
+        tooltip, highlighters.asScala))
+    })
+  }
+}
