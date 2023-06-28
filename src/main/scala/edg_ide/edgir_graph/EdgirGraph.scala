fb0790c3a9af107a9dc30c96f827de9cb5d7cf3e
Richard Lin <elpato25@gmail.com>
2023-06-28 00:20:20 -0700

    Update EdgirGraph.scala

--- src/main/scala/edg_ide/edgir_graph/EdgirGraph.scala
+++ src/main/scala/edg_ide/edgir_graph/EdgirGraph.scala
@@ -19,7 +19,6 @@ case class BlockWrapper(path: DesignPath, blockLike: elem.BlockLike) extends Nod
     case elem.BlockLike.Type.Hierarchy(block) =>
       block.getSelfClass.toSimpleString
     case elem.BlockLike.Type.LibElem(lib) =>
-      require(lib.mixins.isEmpty) // TODO support mixins
       val allTypes = Seq(lib.getBase) ++ lib.mixins
       s"lib: ${allTypes.map(_.toSimpleString).mkString("+")}"
     case other => other.getClass.getName
