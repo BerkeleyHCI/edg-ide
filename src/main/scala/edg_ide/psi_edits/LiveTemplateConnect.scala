28fdebd47a5924f9ff6a8634abe4f48438ead5cb
Richard Lin <elpato25@gmail.com>
2023-08-28 00:38:00 -0700

    Update LiveTemplateConnect.scala

--- src/main/scala/edg_ide/psi_edits/LiveTemplateConnect.scala
+++ src/main/scala/edg_ide/psi_edits/LiveTemplateConnect.scala
@@ -5,10 +5,20 @@ import com.intellij.openapi.command.WriteCommandAction.writeCommandAction
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
-import com.jetbrains.python.psi.{PyClass, PyFunction, PyStatement}
+import com.jetbrains.python.psi.{
+  LanguageLevel,
+  PyAssignmentStatement,
+  PyCallExpression,
+  PyClass,
+  PyElementGenerator,
+  PyFunction,
+  PyStatement,
+  PyStatementList,
+  PyTargetExpression
+}
 import edgir.elem.elem
 import edg.util.Errorable
-import edg_ide.util.ExceptionNotifyImplicits.{ExceptNotify, ExceptOption}
+import edg_ide.util.ExceptionNotifyImplicits.{ExceptNotify, ExceptOption, ExceptSeq}
 import edg_ide.util.{ConnectBuilder, PortConnects, exceptable, requireExcept}
 
 object LiveTemplateConnect {
@@ -22,21 +32,46 @@ object LiveTemplateConnect {
       newConnects: Seq[PortConnects.Base],
       continuation: (String, PsiElement) => Unit
   ): Errorable[() => Unit] = exceptable {
+    val languageLevel = LanguageLevel.forElement(contextClass)
+    val psiElementGenerator = PyElementGenerator.getInstance(project)
 
     val movableLiveTemplate = new MovableLiveTemplate(actionName) {
       // TODO startTemplate should be able to fail - Errorable
       override def startTemplate(caretEltOpt: Option[PsiElement]): InsertionLiveTemplate = {
-        val caretElt = caretEltOpt.exceptNone("no elt at caret")
-        val caretStatement = InsertAction.snapInsertionEltOfType[PyStatement](caretElt).get
-        val containingPsiFn = PsiTreeUtil
-          .getParentOfType(caretStatement, classOf[PyFunction])
-          .exceptNull(s"caret not in a function")
-        val containingPsiClass = PsiTreeUtil
-          .getParentOfType(containingPsiFn, classOf[PyClass])
-          .exceptNull(s"caret not in a class")
-        requireExcept(containingPsiClass == contextClass, s"caret not in class of type ${contextClass.getName}")
-        caretStatement
-        ???
+        val validCaretEltOpt = caretEltOpt.flatMap(TemplateUtils.getInsertionStmt(_, contextClass))
+        // TODO support append to existing connect
+        val insertAfter = validCaretEltOpt
+          .getOrElse(InsertAction.findInsertionElements(contextClass, InsertBlockAction.VALID_FUNCTION_NAMES).head)
+
+        val containingPsiFn = PsiTreeUtil.getParentOfType(insertAfter, classOf[PyFunction])
+        val containingPsiClass = PsiTreeUtil.getParentOfType(containingPsiFn, classOf[PyClass])
+        val selfName = containingPsiFn.getParameterList.getParameters.toSeq
+          .exceptEmpty(s"function ${containingPsiFn.getName} has no self")
+          .head
+          .getName
+        val containingStmtList = PsiTreeUtil.getParentOfType(insertAfter, classOf[PyStatementList])
+
+        val newConnectTemplate = psiElementGenerator.createFromText(
+          languageLevel,
+          classOf[PyAssignmentStatement],
+          s"$selfName.name = $selfName.connect()"
+        )
+        val newConnect =
+          containingStmtList.addAfter(newConnectTemplate, insertAfter).asInstanceOf[PyAssignmentStatement]
+        val newConnectArgs = newConnect.getAssignedValue
+          .asInstanceOf[PyCallExpression]
+          .getArgumentList
+
+        // TODO ADD ALL THE CONNECTS
+
+        val nameTemplateVar = new InsertionLiveTemplate.Reference(
+          "name",
+          newConnect.getTargets.head.asInstanceOf[PyTargetExpression],
+          InsertionLiveTemplate.validatePythonName(_, _, Some(containingPsiClass)),
+          defaultValue = Some("")
+        )
+
+        new InsertionLiveTemplate(newConnect, IndexedSeq(nameTemplateVar))
       }
     }
 
