cd7bf0cfbde5579df34cd1d8d2ffd10fa7d1a484
Richard Lin <elpato25@gmail.com>
2024-03-30 20:02:01 -0700

    Update CompileProcessHandler.scala

--- src/main/scala/edg_ide/runner/CompileProcessHandler.scala
+++ src/main/scala/edg_ide/runner/CompileProcessHandler.scala
@@ -276,9 +276,18 @@ class CompileProcessHandler(
 ) extends ProcessHandler
     with HasConsoleStages {
   var runThread: Option[Thread] = None
+  var pythonInterfaceOpt: Option[LoggingPythonInterface] = None
 
   override def destroyProcessImpl(): Unit = {
+    pythonInterfaceOpt.foreach { pythonInterface =>
+      pythonInterface.destroy()
+      pythonInterface.forwardProcessOutput()
+      console.print(f"Python subprocess terminated.\n", ConsoleViewContentType.ERROR_OUTPUT)
+      pythonInterfaceOpt = None
+    }
     runThread.foreach(_.interrupt())
+    console.print(f"Compilation terminated.\n", ConsoleViewContentType.ERROR_OUTPUT)
+    terminatedNotify(-1)
   }
   override def detachProcessImpl(): Unit = {
     throw new NotImplementedError()
@@ -303,7 +312,7 @@ class CompileProcessHandler(
     BlockVisualizerService(project).setDesignStale()
 
     // A maybe Python interface is needed at this level so on an error it can be shut down if it exists
-    var pythonInterfaceOpt: Option[LoggingPythonInterface] = None
+    require(pythonInterfaceOpt.isEmpty)
     var exitCode: Int = -1
 
     try {
