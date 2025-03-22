a9bf825242130a29877bc1398df3a8cf4e8bfc98
Richard Lin <elpato25@gmail.com>
2025-03-22 00:54:05 -0700

    Update EdgirGraphTest.scala

--- src/test/scala/edg_ide/edgir_graph/tests/EdgirGraphTest.scala
+++ src/test/scala/edg_ide/edgir_graph/tests/EdgirGraphTest.scala
@@ -285,10 +285,16 @@ class EdgirGraphTest extends AnyFlatSpec with Matchers {
           data = BlockWrapper(DesignPath() + "block", designIr.blocks("block")),
           members = SeqMap(
             Seq("ports", "0") -> EdgirGraph.EdgirPort(
-              data = PortWrapper(DesignPath() + "block" + "ports" + "0", blockIr.ports("ports").getArray.getPorts.ports.head.getValue)
+              data = PortWrapper(
+                DesignPath() + "block" + "ports" + "0",
+                blockIr.ports("ports").getArray.getPorts.ports.head.getValue
+              )
             ),
             Seq("ports", "test") -> EdgirGraph.EdgirPort(
-              data = PortWrapper(DesignPath() + "block" + "ports" + "test", blockIr.ports("ports").getArray.getPorts.ports.last.getValue)
+              data = PortWrapper(
+                DesignPath() + "block" + "ports" + "test",
+                blockIr.ports("ports").getArray.getPorts.ports.last.getValue
+              )
             ),
           ),
           edges = Seq()
